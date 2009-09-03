667b344e08336ceaee0c3816780ac31a404026a0
lifesinger <lifesinger@170831de-690a-11de-a84c-2f1c77ec4fbd>
2009-09-03 14:15:24 +0000

    add clipboard

---
+++ src/clipboard/clipboard.js
@@ -0,0 +1,41 @@
+/**
+ * KISSY.Clipboard 剪贴板工具
+ *
+ * clipboard.js
+ * requires: yahoo-dom-event
+ *
+ * @author lifesinger@gmail.com
+ */
+
+var KISSY = window.KISSY || {};
+
+(function() {
+    var Y = YAHOO.util, Dom = Y.Dom, Event = Y.Event, Lang = YAHOO.lang;
+
+    var ClipBoard = {
+
+        /**
+         * 设置数据
+         * @param {string} text
+         */
+        setData: function(text) {
+
+        },
+
+        /**
+         * 获取数据
+         * @return {string}
+         */
+        getData: function() {
+
+        },
+
+        /**
+         * 清空剪贴板
+         */
+        clearData: function() {
+
+        }
+    };
+
+})();
