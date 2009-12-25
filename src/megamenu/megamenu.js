5aa017fa2b3e1a6e5574e09c4aaa906e409d6e03
lifesinger <lifesinger@170831de-690a-11de-a84c-2f1c77ec4fbd>
2009-12-25 06:40:22 +0000

    normal update

--- src/megamenu/megamenu.js
+++ src/megamenu/megamenu.js
@@ -2,36 +2,31 @@
  * 超级菜单组件
  * @module      megamenu
  * @creator     玉伯<lifesinger@gmail.com>
- * @depends     kissy-core, yahoo-dom-event
+ * @depends     kissy-core, yui-base
  */
 KISSY.add("megamenu", function(S) {
 
     var Y = YAHOO.util, Dom = Y.Dom, Event = Y.Event, Lang = YAHOO.lang,
         NONE = "none", BLOCK = "block",
-        CLOSEBTN_TMPL = '<span class="{hook_cls}"></span>',
+        CLOSEBTN_TMPL = "<span class=\"{hook_cls}\"></span>",
         BEFORE_SHOW = "beforeShow",
         ON_HIDE = "onHide",
+        CLS_PREFIX = "ks-megamenu-",
 
         defaultConfig = {
 
-            /**
-             * 延迟时间
-             *  [x,y] - x 表示显示时的延迟，y 表示隐藏时的延迟
-             *  x     - 表示上面的 y 和 x 一样
-             */
-            delay: [.1, .5],
+            triggerType: "mouse", // or "click" 触发类型
+            triggerDelay: 0.1, // 触发延迟
+            hideDelay: .5,    // 隐藏延迟
 
-            // class
-            itemCls: "ks-megamenu-item",
-            viewCls: "ks-megamenu-view",
-            contentCls: "ks-megamenu-content",
-            currentItemCls: "ks-megamenu-item-current",
-            closeBtnCls: "ks-megamenu-closebtn",
+            // 只支持适度灵活结构，view 可以自动生成
+            itemCls: CLS_PREFIX + "item",
+            viewCls: CLS_PREFIX + "view",
+            contentCls: CLS_PREFIX + "content",
+            activeItemCls: CLS_PREFIX + "item-active",
+            closeBtnCls: CLS_PREFIX + "closebtn",
 
-            /**
-             * 是否显示关闭按钮
-             */
-            showCloseBtn: true
+            showCloseBtn: true // 是否显示关闭按钮
         };
 
     /**
@@ -39,8 +34,11 @@ KISSY.add("megamenu", function(S) {
      * @constructor
      */
     function MegaMenu(container, config) {
+        var self = this,
+            cfg = S.merge(defaultConfig, config || {});
+        
         // factory or constructor
-        if (!(this instanceof arguments.callee)) {
+        if (!(self instanceof arguments.callee)) {
             return new arguments.callee(container, config);
         }
 
@@ -48,69 +46,66 @@ KISSY.add("megamenu", function(S) {
          * 菜单容器
          * @type HTMLElement
          */
-        this.container = Dom.get(container);
+        self.container = Dom.get(container);
 
         /**
          * 配置参数
          * @type Object
          */
-        var cfg = S.merge(defaultConfig, config || {});
-        cfg.delay[0] *= 1000;
-        cfg.delay[1] *= 1000;
-        this.config = cfg;
+        self.config = cfg;
 
         /**
          * 菜单项
          */
-        this.items = Dom.getElementsByClassName(cfg.itemCls, "*", container);
+        self.triggers = Dom.getElementsByClassName(cfg.itemCls, "*", container);
 
         /**
          * 显示容器
          */
-        this.view = Dom.getElementsByClassName(cfg.viewCls, "*", container)[0];
-        this.view.contentEl = this.view; // dataEl 是放置数据的容器，无关闭按钮时，就是 view 本身。
+        self.view = Dom.getElementsByClassName(cfg.viewCls, "*", container)[0];
+        self.view.contentEl = self.view; // contentEl 是放置数据的容器，无关闭按钮时，就是 view 本身。
 
         /**
          * 内容
          */
-        this.contents = [];
+        self.contents = [];
         Dom.getElementsByClassName(cfg.contentCls, "*", container, function(each) {
-            this.contents.push(each.value || each.innerHTML);
-        }, this, true);
+            self.contents.push(each.value || each.innerHTML);
+        });
 
         /**
          * 定时器
          */
-        this.showTimer = null;
-        this.hideTimer = null;
+        self.showTimer = null;
+        self.hideTimer = null;
 
         /**
          * 当前激活项
          */
-        this.activeIndex = -1;
+        self.activeIndex = -1;
 
-        // go
-        this._init();
+        // init
+        self._init();
     }
 
     S.mix(MegaMenu.prototype, {
 
         _init: function() {
-            var o = this;
+            var self = this;
 
-            if(o.config.showCloseBtn){
-                o._initCloseBtn();
+            if(self.config.showCloseBtn){
+                self._initCloseBtn();
             }
 
-            o._bindUI();
+            self._bindUI();
         },
 
         _initCloseBtn: function() {
-            var o = this, el, view = o.view;
+            var self = this, el, view = self.view;
 
-            view.innerHTML = CLOSEBTN_TMPL.replace("{hook_cls}", o.config.closeBtnCls);
+            view.innerHTML = CLOSEBTN_TMPL.replace("{hook_cls}", self.config.closeBtnCls);
             Event.on(view.firstChild, "click", function() {
-                o.hide();
+                self.hide();
             });
 
             el = document.createElement("div");
@@ -119,8 +114,8 @@ KISSY.add("megamenu", function(S) {
         },
 
         _bindUI: function() {
-            var o = this, items = o.items, view = o.view,
-                delay = o.config.delay, i, len = o.items.length;
+            var o = this, items = o.triggers, view = o.view,
+                delay = o.config.delay, i, len = o.triggers.length;
 
             for (i = 0; i < len; i++) {
                 (function(index) {
@@ -156,7 +151,7 @@ KISSY.add("megamenu", function(S) {
         show: function(index) {
             var o = this, view = o.view,
                 activeIndex = o.activeIndex,
-                curCls = o.config.currentItemCls;
+                curCls = o.config.activeItemCls;
 
             // bugfix: YAHOO.lang.later 里的 d = d || [];
             index = index || 0;
@@ -173,9 +168,9 @@ KISSY.add("megamenu", function(S) {
 
             // toggle current item
             if(activeIndex >= 0) {
-                Dom.removeClass(o.items[activeIndex], curCls);
+                Dom.removeClass(o.triggers[activeIndex], curCls);
             }
-            Dom.addClass(o.items[index], curCls);
+            Dom.addClass(o.triggers[index], curCls);
 
             // load new content
             o.updateContent(index);
@@ -188,7 +183,7 @@ KISSY.add("megamenu", function(S) {
             var o = this;
 
             // hide current
-            Dom.removeClass(o.items[o.activeIndex], o.config.currentItemCls);
+            Dom.removeClass(o.triggers[o.activeIndex], o.config.activeItemCls);
             o.view.style.display = NONE;
 
             // update
