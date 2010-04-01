ae707d588b838f4b4556797bf6c1f562a61026bc
lifesinger <lifesinger@170831de-690a-11de-a84c-2f1c77ec4fbd>
2010-04-01 13:19:04 +0000

    add NodeList

--- build/node/node-pkg.js
+++ build/node/node-pkg.js
@@ -1,3 +1,8 @@
+/*
+Copyright 2010, KISSY UI Library v1.0.5
+MIT Licensed
+build: 513 Apr 1 21:17
+*/
 /**
  * @module  node
  * @author  lifesinger@gmail.com
@@ -102,3 +107,71 @@ KISSY.add('node', function(S) {
 
     S.Node = Node;
 });
+/**
+ * @module  nodelist
+ * @author  lifesinger@gmail.com
+ * @depends kissy, dom
+ */
+
+KISSY.add('nodelist', function(S) {
+
+    var DOM = S.DOM,
+        push = Array.prototype.push,
+        NP = NodeList.prototype;
+
+    /**
+     * The NodeList class provides a wrapper for manipulating DOM NodeList.
+     */
+    function NodeList(domNodes) {
+        // factory or constructor
+        if (!(this instanceof NodeList)) {
+            return new NodeList(domNodes);
+        }
+
+        // push nodes
+        push.apply(this, domNodes || []);
+    }
+
+    S.mix(NP, {
+
+        /**
+         * 默认长度为 0
+         */
+        length: 0,
+
+        /**
+         * Applies the given function to each Node in the NodeList. 
+         * @param fn The function to apply. It receives 3 arguments: the current node instance, the node's index, and the NodeList instance
+         * @param context An optional context to apply the function with Default context is the current Node instance
+         */
+        each: function(fn, context) {
+            var len = this.length, i = 0, node;
+            for (; i < len; ++i) {
+                node = new S.Node(this[i]);
+                fn.call(context || node, node, i, this);
+            }
+            return this;
+        }
+    });
+
+    // query api
+    S.all = function(selector, context) {
+        return new NodeList(S.query(selector, context, true));
+    };
+
+    S.NodeList = NodeList;
+});
+
+/**
+ * Notes:
+ *
+ *  2010.04
+ *   - each 方法传给 fn 的 this, 在 jQuery 里指向原生对象，这样可以避免性能问题。
+ *     但从用户角度讲，this 的第一直觉是 $(this), kissy 和 yui3 保持一致，牺牲
+ *     性能，一切易用为首。
+ *   - 有了 each 方法，似乎不再需要 import 所有 dom 方法，意义不大。
+ *   - dom 是低级 api, node 是中级 api, 这是分层的一个原因。还有一个原因是，如果
+ *     直接在 node 里实现 dom 方法，则不大好将 dom 的方法耦合到 nodelist 里。可
+ *     以说，技术成本会制约 api 设计。
+ *
+ */
--- src/node/nodelist.js
+++ src/node/nodelist.js
@@ -24,10 +24,25 @@ KISSY.add('nodelist', function(S) {
     }
 
     S.mix(NP, {
+
         /**
          * 默认长度为 0
          */
-        length: 0
+        length: 0,
+
+        /**
+         * Applies the given function to each Node in the NodeList. 
+         * @param fn The function to apply. It receives 3 arguments: the current node instance, the node's index, and the NodeList instance
+         * @param context An optional context to apply the function with Default context is the current Node instance
+         */
+        each: function(fn, context) {
+            var len = this.length, i = 0, node;
+            for (; i < len; ++i) {
+                node = new S.Node(this[i]);
+                fn.call(context || node, node, i, this);
+            }
+            return this;
+        }
     });
 
     // query api
@@ -37,3 +52,17 @@ KISSY.add('nodelist', function(S) {
 
     S.NodeList = NodeList;
 });
+
+/**
+ * Notes:
+ *
+ *  2010.04
+ *   - each 方法传给 fn 的 this, 在 jQuery 里指向原生对象，这样可以避免性能问题。
+ *     但从用户角度讲，this 的第一直觉是 $(this), kissy 和 yui3 保持一致，牺牲
+ *     性能，一切易用为首。
+ *   - 有了 each 方法，似乎不再需要 import 所有 dom 方法，意义不大。
+ *   - dom 是低级 api, node 是中级 api, 这是分层的一个原因。还有一个原因是，如果
+ *     直接在 node 里实现 dom 方法，则不大好将 dom 的方法耦合到 nodelist 里。可
+ *     以说，技术成本会制约 api 设计。
+ *
+ */
