3d12bca49a8d58f448ba15a86d8cd83daccc3808
lifesinger <lifesinger@170831de-690a-11de-a84c-2f1c77ec4fbd>
2010-06-16 07:18:23 +0000

    add dom-create

--- src/dom/dom-create.js
+++ src/dom/dom-create.js
@@ -5,56 +5,67 @@
 KISSY.add('dom-create', function(S) {
 
     var doc = document,
-        DOM = S.DOM, UA = S.UA,
+        DOM = S.DOM, UA = S.UA, ie = UA.ie,
         DIV = 'div',
         PARENT_NODE = 'parentNode',
         DEFAULT_DIV = doc.createElement(DIV),
-        RE_TAG = /<([a-z]+)/i,
-        RE_SIMPLE_TAG = /^[a-z]+$/i;
+        RE_TAG = /<(\w+)/,
+        RE_SIMPLE_TAG = /^<(\w+)\s*\/?>(?:<\/\1>)?$/;
 
     S.mix(DOM, {
 
         /**
          * Creates a new HTMLElement using the provided html string.
          */
-        create: function(html, ownerDoc) {
+        create: function(html, props, ownerDoc) {
             if (!(html = S.trim(html))) return null;
 
-            // simple tag, such as DOM.create('p')
-            if (RE_SIMPLE_TAG.test(html)) {
-                return (ownerDoc || doc).createElement(html);
-            }
+            var ret = null, creators = DOM._creators,
+                m, tag = DIV, k, nodes;
 
-            var ret = null,
-                creators = DOM._creators,
-                m = RE_TAG.exec(html),
-                tag = DIV, k, nodes;
-
-            if (m && (k = m[1]) && S.isFunction(creators[(k = k.toLowerCase())])) {
-                tag = k;
+            // 简单 tag, 比如 DOM.create('<p>')
+            if ((m = RE_SIMPLE_TAG.exec(html))) {
+                ret = (ownerDoc || doc).createElement(m[1]);
             }
-            nodes = creators[tag](html, ownerDoc);
-
-            if (nodes.length === 1) {
-                // return single node, breaking parentNode ref from "fragment"
-                ret = nodes[0].parentNode.removeChild(nodes[0]);
-            }
+            // 复杂情况，比如 DOM.create('<img src="sprite.png" />')
             else {
-                // return multiple nodes as a fragment
-                ret = nl2frag(nodes, ownerDoc || doc);
+                if ((m = RE_TAG.exec(html)) && (k = m[1]) && S.isFunction(creators[(k = k.toLowerCase())])) {
+                    tag = k;
+                }
+
+                nodes = creators[tag](html, ownerDoc).childNodes;
+
+                if (nodes.length === 1) {
+                    // return single node, breaking parentNode ref from "fragment"
+                    ret = nodes[0][PARENT_NODE].removeChild(nodes[0]);
+                }
+                else {
+                    // return multiple nodes as a fragment
+                    ret = nl2frag(nodes, ownerDoc || doc);
+                }
             }
 
-            return ret;
+            return attachProps(ret, props);
         },
 
         _creators: {
-            'div': function(html, ownerDoc) {
-                var div = ownerDoc ? ownerDoc.createElement(DIV) : DEFAULT_DIV;
-                div.innerHTML = html;
-                return div.childNodes;
+            div: function(html, ownerDoc) {
+                var frag = ownerDoc ? ownerDoc.createElement(DIV) : DEFAULT_DIV;
+                frag.innerHTML = html;
+                return frag;
             }
         }
     });
+
+    // 添加成员到元素中
+    function attachProps(elem, props) {
+        if (DOM._isElementNode(elem) && props) {
+            for (var p in props) {
+                DOM.attr(elem, p, props[p]);
+            }
+        }
+        return elem;
+    }
 
     // 将 nodeList 转换为 fragment
     function nl2frag(nodes, ownerDoc) {
@@ -68,12 +79,12 @@ KISSY.add('dom-create', function(S) {
                 nodes = S.makeArray(nodes);
             }
 
-            for (i = 0, len = nodes.length; i < len; i++) {
+            for (i = 0,len = nodes.length; i < len; i++) {
                 ret.appendChild(nodes[i]);
             }
         }
         else {
-            S.error('Unable to convert ' + nodes + ' to fragment.');
+            S.log('Unable to convert ' + nodes + ' to fragment.');
         }
 
         return ret;
@@ -82,59 +93,57 @@ KISSY.add('dom-create', function(S) {
     // 定义 creators, 处理浏览器兼容
     var creators = DOM._creators,
         create = DOM.create,
-        RE_TBODY = /(?:\/(?:thead|tfoot|tbody|caption|col|colgroup)>)+\s*<tbody/;
+        TABLE_OPEN = '<table>',
+        TABLE_CLOSE = '</table>',
+        RE_TBODY = /(?:\/(?:thead|tfoot|caption|col|colgroup)>)+\s*<tbody/,
+        creatorsMap = {
+            option: 'select',
+            td: 'tr',
+            tr: 'tbody',
+            tbody: 'table',
+            col: 'colgroup',
+            legend: 'fieldset' // ie 支持，但 gecko 不支持
+        };
 
-    if (UA.ie && UA.ie < 8) {
-        S.mix(creators, {
-            // IE adds TBODY when creating TABLE elements (which may share this impl)
-            tbody: function(html, ownerDoc) {
-                var table = create('<table>' + html + '</table>', ownerDoc),
-                    tbody = table.children.tags('tbody')[0];
-
-                if (table.children.length > 1 && tbody && !RE_TBODY.test(html)) {
-                    tbody[PARENT_NODE].removeChild(tbody); // strip extraneous tbody
+    if (UA.gecko || ie) {
+        for (var p in creatorsMap) {
+            (function(tag) {
+                creators[p] = function(html, ownerDoc) {
+                    return create('<' + tag + '>' + html + '</' + tag + '>', null, ownerDoc);
                 }
-                return frag;
-            },
+            })(creatorsMap[p]);
+        }
 
-            script: function(html, doc) {
-                var frag = doc.createElement('div');
-
+        if (ie) {
+            // IE 下不能单独添加 script 元素
+            creators.script = function(html, ownerDoc) {
+                var frag = ownerDoc ? ownerDoc.createElement(DIV) : DEFAULT_DIV;
                 frag.innerHTML = '-' + html;
-                frag.removeChild(frag[FIRST_CHILD]);
+                frag.removeChild(frag.firstChild);
                 return frag;
+            };
+
+            // IE7- adds TBODY when creating thead/tfoot/caption/col/colgroup elements
+            if (ie < 8) {
+                creators.tbody = function(html, ownerDoc) {
+                    var frag = create(TABLE_OPEN + html + TABLE_CLOSE, null, ownerDoc),
+                        tbody = frag.children['tags']('tbody')[0];
+
+                    if (frag.children.length > 1 && tbody && !RE_TBODY.test(html)) {
+                        tbody[PARENT_NODE].removeChild(tbody); // strip extraneous tbody
+                    }
+                    return frag;
+                };
             }
-        });
-    }
-
-    if (UA.gecko || UA.ie) {
-        S.mix(creators, {
-            option: function(html, doc) {
-                return create('<select>' + html + '</select>', doc);
-            },
-
-            tr: function(html, doc) {
-                return create('<tbody>' + html + '</tbody>', doc);
-            },
-
-            td: function(html, doc) {
-                return create('<tr>' + html + '</tr>', doc);
-            },
-
-            tbody: function(html, doc) {
-                return create(TABLE_OPEN + html + TABLE_CLOSE, doc);
-            }
-        });
+        }
 
         S.mix(creators, {
-            legend: 'fieldset',
+            optgroup: creators.option, // gecko 支持，但 ie 不支持
             th: creators.td,
             thead: creators.tbody,
             tfoot: creators.tbody,
             caption: creators.tbody,
-            colgroup: creators.tbody,
-            col: creators.tbody,
-            optgroup: creators.option
+            colgroup: creators.tbody
         });
     }
 });
@@ -143,4 +152,5 @@ KISSY.add('dom-create', function(S) {
  * TODO:
  *  - 研究 jQuery 的 buildFragment 和 clean
  *  - 增加 cache, 完善 test cases
+ *  - 支持更多 props
  */
