a5c9c2d1579affd64f82455c9de81dacdef9a463
cubee <lijing0033@souhu.com>
2010-08-12 21:37:53 +0800

    loader 更新

--- src/kissy/kissy.js
+++ src/kissy/kissy.js
@@ -113,7 +113,7 @@
 			//when a module is add to KISSY via S.add() after "domReady" event,
 			//its callback function should be exec immediately
 			if(!(isReady && !afterReady)){
-				console.log('=======' + name + ' ' + isReady);
+				console.log('============'+name + isReady + ' '+ afterReady);
 				fn(self);
 			}
 
@@ -258,9 +258,9 @@
 
 					// Reset the list of functions
 					readyList = null;
+					afterReady = true;
 					
 				});
-				afterReady = true;
 
             }
         },
