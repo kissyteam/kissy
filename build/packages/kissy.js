846ec17ab12467b88899a5807f57f0d26cf25147
lifesinger <lifesinger@gmail.com>
2010-08-21 11:04:39 +0800

    bug fix for loader

--- build/packages/kissy.js
+++ build/packages/kissy.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2010, KISSY UI Library v1.1.2
 MIT Licensed
-build time: Aug 21 10:38
+build time: Aug 21 11:03
 */
 /**
  * @module kissy
@@ -1073,9 +1073,16 @@ build time: Aug 21 10:38
         },
 
         __mixMod: function(mods, gMods, name, global) {
-            var mod = mods[name] || { };
+            var mod = mods[name] || { }, status = mod.status;
+            
             S.mix(mod, S.clone(gMods[name]));
-            if(global) this.__buildPath(mod, global.Config.base); // 来自 global 的 mod, path 应该基于 global
+
+            // status 属于实例，当有值时，不能被覆盖。只有没有初始值时，才从 global 上继承
+            if(status) mod.status = status;
+
+            // 来自 global 的 mod, path 应该基于 global
+            if(global) this.__buildPath(mod, global.Config.base);
+
             mods[name] = mod;
         },
 
--- src/kissy/loader.js
+++ src/kissy/loader.js
@@ -182,9 +182,16 @@
         },
 
         __mixMod: function(mods, gMods, name, global) {
-            var mod = mods[name] || { };
+            var mod = mods[name] || { }, status = mod.status;
+            
             S.mix(mod, S.clone(gMods[name]));
-            if(global) this.__buildPath(mod, global.Config.base); // 来自 global 的 mod, path 应该基于 global
+
+            // status 属于实例，当有值时，不能被覆盖。只有没有初始值时，才从 global 上继承
+            if(status) mod.status = status;
+
+            // 来自 global 的 mod, path 应该基于 global
+            if(global) this.__buildPath(mod, global.Config.base);
+
             mods[name] = mod;
         },
 
