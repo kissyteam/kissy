441a134444ae926cdd548792ffcbaa15dbe01186
yiminghe <yiminghe@gmail.com>
2010-09-13 10:18:17 +0800

    refactor ant build

--- tools/build-all.sh
+++ tools/build-all.sh
@@ -3,17 +3,7 @@
 #1.chmod 777 build.sh
 #2.chmod 777 ant/bin/ant
 ANT=../../kissy-tools/ant/bin/ant
-
-src_files=`find ../src -name build.xml`
-build_files=`find ../build -name build.xml`
-for file in $src_files
-do
-    $ANT -buildfile $file
-done
-
-for file in $build_files
-do
-    $ANT -buildfile $file
+$ANT -buildfile run.xml build-all
 done
 
 
