067f914c9e9abe2c064afe97ed9b9f100ef3de39
lifesinger <lifesinger@gmail.com>
2010-09-20 18:10:24 +0800

    add jasmine & json test spec

--- src/json/tests/json-spec.js
+++ src/json/tests/json-spec.js
@@ -1,34 +1,25 @@
-
 describe("JSON", function() {
 
     var JSON = KISSY.JSON;
 
-    describe("JSON.stringify", function() {
+    it("should stringify input properly", function() {
         var stringify = JSON.stringify;
 
-        it("should stringify input properly", function() {
-
-            expect(stringify({'a': true})).toBe('{"a":true}');
-
-            expect(stringify(true)).toBe('true');
-            expect(stringify(null)).toBe('null');
-            expect(stringify(undefined)).toBe(undefined);
-            expect(stringify(NaN)).toBe('null');
-        });
+        expect(stringify({'a': true})).toBe('{"a":true}');
+        expect(stringify(true)).toBe('true');
+        expect(stringify(null)).toBe('null');
+        expect(stringify(undefined)).toBe(undefined);
+        expect(stringify(NaN)).toBe('null');
     });
-    
-    describe("JSON.parse", function() {
+
+
+    it("should parse input properly", function() {
         var parse = JSON.parse;
 
-        it("should parse input properly", function() {
-
-            expect(parse('{"a":true}').a).toBe(true);
-
-            expect(parse('true')).toBe(true);
-            expect(parse(true)).toBe(true);
-
-            expect(parse(null)).toBe(null);
-            expect(parse('null')).toBe(null);
-        });
+        expect(parse('{"a":true}').a).toBe(true);
+        expect(parse('true')).toBe(true);
+        expect(parse(true)).toBe(true);
+        expect(parse(null)).toBe(null);
+        expect(parse('null')).toBe(null);
     });
 });
