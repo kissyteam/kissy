12c765989d71317faa0f754526ef1a717441d72c
yiminghe <yiminghe@gmail.com>
2010-11-09 20:12:05 +0800

    start ext for overlay

--- src/base/base.js
+++ src/base/base.js
@@ -3,7 +3,9 @@
  * @author  lifesinger@gmail.com, yiminghe@gmail.com
  */
 KISSY.add('base', function (S) {
-
+    var UI_SET = "_uiSet",
+        SRC_NODE = "srcNode",
+        capitalFirst = S.Attribute.capitalFirst;
     /*
      * Base for class-based component
      */
@@ -11,13 +13,17 @@ KISSY.add('base', function (S) {
         var self = this;
         initHierarchy(self, config);
         self.init && self.init();
+        config && config.autoRender && self.renderer();
     }
 
     /**
      * init attr using constructors ATTRS meta info
      */
     function initHierarchy(host, config) {
-        var c = host.constructor, attr, attrs, ATTRS = 'ATTRS';
+        var c = host.constructor,
+            attr,
+            attrs,
+            ATTRS = 'ATTRS';
 
         // define
         while (c) {
@@ -37,14 +43,20 @@ KISSY.add('base', function (S) {
             exts = exts && exts._exts;
             if (exts) {
                 for (var i = 0; i < exts.length; i++) {
-                    exts[i].call(host, config);
+                    exts[i] && exts[i].call(host, config);
                 }
             }
+
+            //从 markup 生成相应的属性项
+            if (config &&
+                config[SRC_NODE] &&
+                c.HTML_PARSER) applyParser.call(host, config[SRC_NODE], c.HTML_PARSER);
 
             c = c.superclass ? c.superclass.constructor : null;
         }
 
         // initialize
+        //注意：用户设置的属性值会覆盖 html_parser 得到的属性值
         if (config) {
             for (attr in config) {
                 if (config.hasOwnProperty(attr))
@@ -53,7 +65,103 @@ KISSY.add('base', function (S) {
         }
     }
 
-    S.augment(Base, S.EventTarget, S.Attribute);
+    function applyParser(srcNode, parser) {
+        var self = this;
+        for (var p in parser) {
+            if (parser.hasOwnProperty(p)) {
+                var v = parser[p];
+                //函数
+                if (S.isFunction(v)) {
+                    self.set(p, v.call(self, srcNode));
+                }
+                //单选选择器
+                else if (S.isString(v)) {
+                    self.set(p, srcNode.one(v));
+                }
+                //多选选择器
+                else if (S.isArray(v) && v[0]) {
+                    self.set(p, srcNode.all(v[0]))
+                }
+            }
+        }
+    }
+
+
+    Base.HTML_PARSER = {};
+    Base.ATTRS = {
+        //保证只会 renderer 一次
+        rendered:{
+            value:false
+        },
+        render:{
+            setter:function(v) {
+                if (S.isString(v))
+                    return S.one(v);
+            }
+        }
+    };
+    S.augment(Base, S.EventTarget, S.Attribute, {
+        renderer:function() {
+            var self = this,
+                render = self.get("render"),
+                rendered = self.get("rendered");
+            if (!rendered) {
+                self.renderUI(render);
+                self.bindUI();
+                self.syncUI();
+                self.set("rendered", true);
+            }
+        },
+        /**
+         * 根据属性添加 DOM 节点
+         */
+        renderUI:function() {
+            this.fire("renderUI");
+        },
+        /**
+         * 根据属性变化设置 UI
+         */
+        bindUI:function() {
+            var self = this,
+                attrs = self.getDefAttrs();
+            for (var a in attrs) {
+                if (attrs.hasOwnProperty(a)) {
+                    var m = UI_SET + capitalFirst(a);
+                    if (self[m]) {
+                        //自动绑定事件到对应函数
+                        (function(a, m) {
+                            self.on("after" + capitalFirst(a) + "Change", function(ev) {
+                                self[m](ev.newVal);
+                            });
+                        })(a, m);
+                    }
+                }
+            }
+
+            self.fire("bindUI");
+        },
+        /**
+         * 根据当前（初始化）状态来设置 UI
+         */
+        syncUI:function() {
+            var self = this,
+                attrs = self.getDefAttrs();
+            for (var a in attrs) {
+                if (attrs.hasOwnProperty(a)) {
+                    var m = UI_SET + capitalFirst(a);
+                    if (self[m]) {
+                        self[m](self.get(a));
+                    }
+                }
+            }
+            self.fire("syncUI");
+        },
+
+        destroy:function() {
+            this.detach();
+            this.fire("destroy");
+        }
+    });
 
 
     /**
@@ -70,9 +178,9 @@ KISSY.add('base', function (S) {
             exts = arguments[0];
             baseCls = Base;
         }
+        baseCls = baseCls || Base;
         function re() {
-            re.superclass.constructor.call(this, arguments);
-            this.init();
+            re.superclass.constructor.apply(this, arguments);
         }
 
         S.extend(re, baseCls, px, sx);
@@ -80,10 +188,20 @@ KISSY.add('base', function (S) {
             re._kissycreate = re._kissycreate || {};
             re._kissycreate._exts = exts;
             for (var i = 0; i < exts.length; i++) {
-                var attrs = exts[i].ATTRS;
-                if (attrs) {
+                var ext = exts[i],
+                    attrs = ext && ext.ATTRS,
+                    parsers = ext && ext.HTML_PARSER;
+                if (attrs && ext) {
+
                     re.ATTRS = re.ATTRS || {};
-                    S.mix(re.ATTRS, attrs);
+                    re.HTML_PARSER = re.HTML_PARSER || {};
+
+                    //合并功能类属性定义到主类，不要覆盖主类属性重定义
+                    S.mix(re.ATTRS, attrs, false);
+                    //合并功能类 htmlparser 定义到主类，不要覆盖主类属性重定义
+                    S.mix(re.HTML_PARSER, parsers, false);
+
+                    //合并功能类代码到主类
                     S.augment(re, exts[i]);
                 }
             }
@@ -94,5 +212,5 @@ KISSY.add('base', function (S) {
 });
 
 /**
- * 2011-11-08 add create by chengyu,Base && yui3 Widget 压缩一下 
+ * 2011-11-08 Base && yui3 Widget 压缩一下，增加扩展类支持，组件初始化生命周期以及 htmlparser
  */
--- src/overlay/overlay.js
+++ src/overlay/overlay.js
@@ -1,235 +1,52 @@
 /**
  * KISSY Overlay
- * @author   玉伯<lifesinger@gmail.com>, 乔花<qiaohua@taobao.com>
+ * @author 玉伯<lifesinger@gmail.com>, 乔花<qiaohua@taobao.com>,yiminghe@gmail.com
  */
-KISSY.add('overlay', function(S, undefined) {
+KISSY.add("overlay", function(S, undefined) {
 
-    var doc = document,
-        DOM = S.DOM, Event = S.Event,
-        ie6 = S.UA.ie === 6,
+    var Base = S.Base,
+        EL = "el",
+        TRIGGERTYPE = "triggerType",
+        MOUSE = "mouse",
+        MOUSEENTER = MOUSE + "enter",
+        MOUSELEAVE = MOUSE + "leave",
+        CLICK = "click";
 
-        DOT = '.', KEYDOWN = 'keydown', EMPTY = '',
-        TRIGGERTYPE = 'triggerType', MOUSE = 'mouse', MOUSEENTER = 'mouseenter', MOUSELEAVE = 'mouseleave', CLICK = 'click',
-        MASK = 'mask', SHOW = 'show', HIDE = 'hide',
-        BDCLS = 'bdCls', CONTENT = 'content', ZINDEX = 'zIndex', DISPLAY = 'display', WIDTH = 'width', HEIGHT = 'height',
-        X = 'x', Y = 'y',
-
-        CLS_CONTAINER = 'ks-overlay',
-        CLS_PREFIX = CLS_CONTAINER + '-',
-
-        EVENT_SHOW = SHOW,
-        EVENT_HIDE = HIDE,
-        EVENT_CREATE = 'create',
-
-        DEFAULT_STYLE = 'visibility:hidden;position:absolute;',
-        TMPL = '<div class="{containerCls}" style="' + DEFAULT_STYLE + '"><div class="{bdCls}">{content}</div></div>',
-
-        mask;
 
     /*
      * DOM 结构
-     *  <div class="ks-overlay-container">
-     *      <div class="ks-overlay-bd"></div>
+     *  <div class="ks-overlay">
      *  </div>
      */
 
-    /*
-     * Overlay Class
-     * @constructor
-     * @param {Element=} container
-     * @param {Object=} config
-     * attached members：
-     *  - this.container
-     *  - this.trigger
-     *  - this.currentTrigger
-     *  - this.body
-     *  - this.shim
-     */
-    function Overlay(container, config) {
-        var self = this;
-        config = config || { };
+    var Overlay = Base.create([S.Ext.Box,S.Ext.InnerBox,S.Ext.Position], {
 
-        // 支持 Overlay(config)
-        if (S.isPlainObject(container)) {
-            config = container;
-        } else {
-            config.container = container;
-        }
-
-        Overlay.superclass.constructor.call(self, config);
-
-        /**
-         * 获取相关联的 DOM 节点
-         * @type {Element}
-         */
-        self.container = S.get(config.container);
-
-        /**
-         * 触发元素, 有可能多个
-         * @type {Array.<Element>}
-         */
-        self.trigger = S.makeArray(S.query(config.trigger));
-
-        /**
-         * 当前触发元素, 默认为第一个
-         * @type {Element}
-         */
-        self.currentTrigger = self.trigger[0];
-
-        self._init();
-    }
-
-    S.extend(Overlay, S.Base);
-
-    Overlay.ATTRS = {
-        container: {            // 容器元素
-            value: null
-        },
-        containerCls: {         // 容器的 class
-            value: CLS_CONTAINER
-        },
-        bdCls: {                // 内容 class
-            value: CLS_PREFIX + 'bd'
-        },
-
-        trigger: {              // 触发器, 可以是多个
-            value: null
-        },
-        triggerType: {          // 触发类型
-            value: CLICK
-        },
-
-        x: {                    // 水平方向绝对位置
-            value: 0
-        },
-        y: {                    // 垂直方向绝对位置
-            value: 0
-        },
-        xy: {                   // 相对 page 定位, 有效值为 [n, m], 为 null 时, 选 align 设置
-            value: null,
-            setter: function(v) {
-                var self = this,
-                    xy = S.makeArray(v);
-
-                if (xy.length) {
-                    xy[0] && self.set(X, xy[0]);
-                    xy[1] && self.set(Y, xy[1]);
-                }
-                return v;
-            }
-        },
-        width: {                // 宽度
-            value: 0,
-            setter: function(v) {
-                return parseInt(v) || 0;
-            }
-        },
-        height: {               // 高度
-            value: 0,
-            setter: function(v) {
-                return parseInt(v) || 0;
-            }
-        },
-
-        content: {              // 内容, 默认为 undefined, 不设置
-            value: undefined
-        },
-
-        mask: {                 // 是否显示蒙层, 默认不显示
-            value: false
-        },
-        shim: {                 // 是否需要垫片
-            value: ie6
-        },
-        zIndex: {
-            value: 9999
-        }
-    };
-
-    Overlay.Plugins = [];
-
-    S.augment(Overlay, {
-        /**
-         * initialize
-         */
-        _init: function() {
-            var self = this;
-
-            self._onAttrChanges();
-
-            if (self.trigger.length > 0) {
+        init:function() {
+            var self = this,trigger = self.get("trigger");
+            if (trigger &&
+                trigger.length > 0) {
                 self._bindTrigger();
             }
-            // init plugins
-            S.each(Overlay.Plugins, function(plugin) {
-                if (plugin.init) {
-                    plugin.init(self);
-                }
-            });
+            self.on("bindUI", self._bindUIOverlay, self);
         },
 
-        _onAttrChanges: function() {
+        /**
+         * bindUI
+         * 注册dom事件以及属性事件
+         * @override
+         */
+        _bindUIOverlay: function() {
             var self = this;
-
-            self.on('afterContainerChange', function(e) {
-                /**
-                 * 获取相关联的 DOM 节点
-                 * @type {Element}
-                 */
-                self.container = S.get(e.newVal);
-            });
-
-            self.on('afterTriggerChange', function(e) {
-                /**
-                 * 触发元素, 有可能多个
-                 * @type {Array.<Element>}
-                 */
-                self.trigger = S.makeArray(S.query(e.newVal));
-                self.currentTrigger = self.trigger[0];
-            });
-
-            self.on('afterXChange', function(e) {
-                var offset = {left: e.newVal};
-
-                DOM.offset(self.container, offset);
-                if (self.shim) self.shim.setOffset(offset);
-            });
-
-            self.on('afterYChange', function(e) {
-                var offset = {top: e.newVal};
-
-                DOM.offset(self.container, offset);
-                if(self.shim) self.shim.setOffset(offset);
-            });
-
-            self.on('afterWidthChange', function(e) {
-                var w = e.newVal;
-                if (w) {
-                    DOM.width(self.container, w);
-                    self.shim && self.shim.setSize(w, self.get(HEIGHT));
-                }
-                // 当设置 0 时, 表示按照内容宽度
-            });
-
-            self.on('afterHeightChange', function(e) {
-                var h = e.newVal;
-                if (h) {
-                    DOM.height(self.container, h);
-                    self.shim && self.shim.setSize(self.get(WIDTH), h);
-                }
-            });
-
-            self.on('afterContentChange', function(e) {
-                DOM.html(self.body, e.newVal);
-            });
+            if (self.get(TRIGGERTYPE) === MOUSE)
+                self._bindElMouse();
         },
+
 
         /**
          * 绑定触发器上的响应事件
          */
         _bindTrigger: function() {
             var self = this;
-
             if (self.get(TRIGGERTYPE) === MOUSE) {
                 self._bindTriggerMouse();
             } else {
@@ -243,25 +60,20 @@ KISSY.add('overlay', function(S, undefined) {
         _bindTriggerMouse: function() {
             var self = this;
 
-            S.each(self.trigger, function(trigger) {
+            self.get("trigger").each(function(trigger) {
                 var timer;
-
-                Event.on(trigger, MOUSEENTER, function() {
+                trigger.on(MOUSEENTER, function() {
                     self._clearHiddenTimer();
-
                     timer = S.later(function() {
-                        self.currentTrigger = trigger;
                         self.show();
                         timer = undefined;
                     }, 100);
                 });
-
-                Event.on(trigger, MOUSELEAVE, function() {
+                trigger.on(MOUSELEAVE, function() {
                     if (timer) {
                         timer.cancel();
                         timer = undefined;
                     }
-
                     self._setHiddenTimer();
                 });
             });
@@ -270,23 +82,17 @@ KISSY.add('overlay', function(S, undefined) {
         /**
          * 下面三个函数, 用于处理鼠标快速移出容器时是否需要隐藏的延时
          */
-        _bindContainerMouse: function() {
-            var self = this;
-
-            Event.on(self.container, MOUSELEAVE, function() {
-                self._setHiddenTimer();
-            });
-
-            Event.on(self.container, MOUSEENTER, function() {
-                self._clearHiddenTimer();
-            });
+        _bindElMouse: function() {
+            var self = this,
+                el = self.get(EL);
+            el.detach(MOUSEENTER + " " + MOUSELEAVE);
+            el.on(MOUSELEAVE, self._setHiddenTimer, self);
+            el.on(MOUSEENTER, self._clearHiddenTimer, self);
         },
 
         _setHiddenTimer: function() {
             var self = this;
-            self._hiddenTimer = S.later(function() {
-                self.hide();
-            }, 120);
+            self._hiddenTimer = S.later(self.hide, 120, false, self);
         },
 
         _clearHiddenTimer: function() {
@@ -302,199 +108,62 @@ KISSY.add('overlay', function(S, undefined) {
          */
         _bindTriggerClick: function() {
             var self = this;
-
-            S.each(self.trigger, function(trigger) {
-                Event.on(trigger, CLICK, function(e) {
+            self.get("trigger").each(function(trigger) {
+                trigger.on(CLICK, function(e) {
                     e.halt();
-                    self.currentTrigger = trigger;
                     self.show();
                 });
             });
         },
 
         /**
-         * 显示 Overlay
-         */
-        show: function() {
-            this._firstShow();
-        },
-
-        /**
-         * 第一次显示时, 需要构建 DOM, 设置位置
-         */
-        _firstShow: function() {
-            var self = this;
-
-            self._prepareMarkup();
-            self._setDisplay();
-            self._setSize();
-            self._setPosition();
-            
-            self.fire(EVENT_CREATE);
-
-            self._realShow();
-            self._firstShow = self._realShow;
-        },
-
-        _realShow: function() {
-            this._toggle(false);
-        },
-
-        /**
-         * 切换显示/隐藏
-         * @param {boolean} isVisible
-         */
-        _toggle: function(isVisible) {
-            var self = this;
-
-            DOM.css(self.container, 'visibility', isVisible ? 'hidden' : EMPTY);
-
-            self.shim && self.shim.toggle();
-            self.get(MASK) && mask[isVisible ? HIDE : SHOW]();
-
-            self[isVisible ? '_unbindUI' : '_bindUI']();
-            self.fire(isVisible ? EVENT_HIDE : EVENT_SHOW);
-        },
-
-        /**
-         * 隐藏
-         */
-        hide: function() {
-            this._toggle(true);
-        },
-
-        _prepareMarkup: function() {
-            var self = this, container = self.container;
-
-            // 多个 Overlay 实例共用一个 mask
-            if (self.get(MASK) && !mask) {
-                mask = new S.Mask();
-            }
-            if (self.get('shim')) {
-                self.shim = new S.Mask({ shim: true });
-            }
-
-            // 已有 Markup
-            if (container) {
-                // 已有 markup 可以很灵活，如果没有 bdCls, 就让 body 指向 container
-                self.body = S.get(DOT + self.get(BDCLS), container) || container;
-
-                container.style.cssText += DEFAULT_STYLE;
-                DOM.html(self.body, self.get(CONTENT));
-            }
-            // 构建 DOM
-            else {
-                container = self.container = DOM.create(S.substitute(TMPL, {
-                    containerCls: self.get('containerCls'),
-                    bdCls: self.get(BDCLS),
-                    content: self.get(CONTENT)
-                }));
-                self.body = DOM.children(container)[0];
-                doc.body.appendChild(container);
-            }
-
-            DOM.css(container, ZINDEX, self.get(ZINDEX));
-
-            if (self.get(TRIGGERTYPE) === MOUSE) self._bindContainerMouse();
-        },
-
-        /**
-         * 防止其他地方设置 display: none 后, 无法再次显示
-         */
-        _setDisplay: function(){
-            var self = this;
-            if (DOM.css(self.container, DISPLAY) === 'none') {
-                DOM.css(self.container, DISPLAY, 'block');
-            }
-        },
-
-        /**
-         * 设置初始宽高
-         */
-        _setSize: function() {
-            var self = this,
-                w, h, container = self.container;
-
-            w = self.get(WIDTH) || DOM.width(container);
-            h = self.get(HEIGHT) || DOM.height(container);
-
-            DOM.width(container, w);
-            DOM.height(container, h);
-            if (self.shim) self.shim.setSize(w, h);
-        },
-        /**
-         * 设置初始位置
-         */
-        _setPosition: function() {
-            var self = this,
-                offset = {left: self.get(X), top: self.get(Y)};
-
-            DOM.offset(self.container, offset);
-            if (self.shim) self.shim.setOffset(offset);
-        },
-
-        /**
-         * 移动到绝对位置上, move(x, y) or move(x) or move([x, y])
-         * @param {number|Array.<number>} x
-         * @param {number=} y
-         */
-        move: function(x, y) {
-            var self = this;
-
-            if (S.isArray(x)) {
-                y = x[1];
-                x = x[0];
-            }
-            self.set(X, x);
-            y && self.set(Y, y);
-        },
-
-        /**
-         * 显示/隐藏时绑定的事件
-         */
-        _bindUI: function() {
-            Event.on(doc, KEYDOWN, this._esc, this);
-        },
-
-        _unbindUI: function() {
-            Event.remove(doc, KEYDOWN, this._esc, this);
-        },
-        
-        _esc: function(e) {
-            if (e.keyCode === 27) this.hide();
-        },
-
-        /**
-         * 设置内容
-         * @param {string} html
-         */
-        setBody: function(html) {
-            this.set(CONTENT, html);
-        },
-
-        /**
          * 删除自己, mask 删不了
          */
-        dispose: function() {
-            var self = this;
+        destroy: function() {
+            var self = this,
+                trigger = self.get("trigger");
+            trigger && trigger.detach(MOUSEENTER +
+                " " +
+                MOUSELEAVE +
+                " " +
+                CLICK);
+            Overlay.superclass.constructor.call(self);
+        }
 
-            self._toggle(true);
-            self.detach();
-            Event.remove(self.container);
-            S.each(self.trigger, function(t) {
-                Event.remove(t, self.get(TRIGGERTYPE) === MOUSE ? (MOUSEENTER + ' ' + MOUSELEAVE) : CLICK);
-            });
-            
-            DOM.remove(self.container);
+    }, {
+        ATTRS : {
+            trigger: {
+                // 触发器, 可以是多个
+                setter:function(v) {
+                    if (S.isString(v)) {
+                        return S.all(v);
+                    }
+                }
+            },
+            triggerType: {
+                // 触发类型
+                value: CLICK
+            },
+
+            //重定义最外层css默认值
+            elCls:{
+                value:"ks-overlay"
+            }
+
         }
     });
 
+
     S.Overlay = Overlay;
 
-}, { requires: ['core'] });
+}, {
+    requires: ["core"]
+});
 
 /**
+ * 2010-11-09 yiminghe 重构，attribute-base-Overlay ，采用 Base.create
+ *
  * TODO:
- *  - stackable ? 
+ *  - stackable ?
  *  - effect
  */
