aa6be881d3bce2ffb1f782434e74373d64f96377
yyfrankyy <yyfrankyy@gmail.com>
2010-11-10 22:12:52 +0800

    重构了代码，内部变量不做任何暴露

--- src/template/template.js
+++ src/template/template.js
@@ -14,68 +14,44 @@ KISSY.add('template', function(S, undefined){
      */
     var Template = function(templ){
 
-        // 获取hash串，作为cache key保留
-        var hex = "Crypto" in S ? S.Crypto.hex_sha1 : S.now,
+        if(!(templ in cache)) {
 
-            _var = '_ks_data',
-
-            _ks_tmpl = "_ks_tmpl_" + hex(templ).substring(0, 13);
-
-        if(templ in cache) {
-            return cache[templ];
-        }
-
-        var _parser = [
-                "var " + _ks_tmpl + " = [];", 
-                "with(" + _var + ") {",
-                    _ks_tmpl + ".push('",
+            var _lq = "<%", _rq = "%>",
+                _parser = [
+                    "var _ks_tmpl=[];with(_ks_data){_ks_tmpl.push('",
                         S.trim(templ)
+                             .replace(new RegExp("[\r\t\n]", "g"), " ")
+                             .replace(new RegExp(_lq, "g"), "\t")
+                             .replace(new RegExp("(^|" + _rq + ")[^\t]*'", "g"), "$1\r")
+                             .replace(new RegExp("\t=(.*?)" + _rq, "g"), "',$1,'")
+                             .replace(new RegExp("\t", "g"), "');")
+                             .replace(new RegExp(_rq, "g"), "_ks_tmpl.push('")
+                             .replace(new RegExp("\r", "g"), "\\'"),
+                    "');};return _ks_tmpl.join('');"
+                ].join("");
 
-                             //把 \' 处理为 \\''
-                             //.replace(/([\\'])/g, "\\$1")
-
-                             //清除换行，和tab，接下去用\t\r进行占位
-                             .replace(/[\r\t\n]/g, " ")
-
-                             //把<%全部替换成\t
-                             .split("<%").join("\t")
-
-                             //XXX 这一步是干嘛的？
-                             .replace(/(^|%>)[^\t]*'/g, "$1\r")
-
-                             //把<%=id%>声明的变量替换为: ',id,'
-                             .replace(/\t=(.*?)%>/g, "',$1,'")
-
-                             //非<%=的\t无用，把之前的push补上括号
-                             //<%表示为真正的js语句，直接执行;
-                             .split("\t").join("');")
-
-                             //上一步替换掉<%，这一步继续替换%>为p.push
-                             //条件/循环等语句内部局部作用域push
-                             .split("%>").join(_ks_tmpl + ".push('")
-
-                             //把换行换成\'，结束多余字符
-                             .split("\r").join("\\'"),
-
-                   "'); ",
-                "}",
-                "return " + _ks_tmpl + ".join('');"
-            ].join("");
-
-        var render = new Function (_var, _parser);
-
-        cache[templ] = {
-            hash: _ks_tmpl,
-            parser: _parser,
-            render: render
-        };
+            cache[templ] = {
+                parser: _parser,
+                render: new Function ("_ks_data", _parser)
+            };
+        }
 
         return cache[templ];
     };
 
-    S.Template = S.mix(Template, {
-        getCache: function(hash){
-            return cache[hash];
-        }
-    });
+    S.Template = Template;
 }, { requires: [ 'core' ] } );
+/**
+ * ChangeLog:
+ *
+ * 2010-9-22
+ *  - 根据new Function的思路，实现了一遍Micro Template，增加一些接口
+ *
+ * 2010-11-02
+ *  - 调整部分接口，实现一个较为复杂的用例，P4P
+ *
+ * 2010-11-10
+ *  - 迁移到kissy目录
+ *  - 按照原来的接口，规划好部分单元测试用例
+ *
+ */
