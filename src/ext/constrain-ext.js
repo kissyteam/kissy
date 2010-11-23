955c814bfb979c647f2129dd8890a65514cbd5a2
yiminghe <yiminghe@gmail.com>
2010-11-23 10:36:34 +0800

    fix constrain ext

--- src/ext/constrain-ext.js
+++ src/ext/constrain-ext.js
@@ -39,10 +39,11 @@ KISSY.add("ext-constrain", function(S) {
         }
         // 没有指定 constrain, 表示受限于可视区域
         else {
+            var vWidth = document.documentElement.clientWidth;
             ret = { left: DOM.scrollLeft(), top: DOM.scrollTop() };
 
             S.mix(ret, {
-                maxLeft: ret.left + DOM.viewportWidth() - el[0].offsetWidth,
+                maxLeft: ret.left + vWidth - el[0].offsetWidth,
                 maxTop: ret.top + DOM.viewportHeight() - el[0].offsetHeight
             });
         }
@@ -56,42 +57,42 @@ KISSY.add("ext-constrain", function(S) {
         },
         _renderUIConstrain:function() {
             S.log("_renderUIConstrain");
+            var self = this,
+                attrs = self.getDefAttrs(),
+                xAttr = attrs["x"],
+                yAttr = attrs["y"],
+                oriXSetter = xAttr["setter"],
+                oriYSetter = yAttr["setter"];
+            xAttr.setter = function(v) {
+                var r = oriXSetter && oriXSetter(v);
+                if (r === undefined) {
+                    r = v;
+                }
+                if (!self.get("constrain")) return r;
+                var _ConstrainExtRegion = _getConstrainRegion.call(
+                    self, self.get("constrain"));
+                return Math.min(Math.max(r,
+                    _ConstrainExtRegion.left),
+                    _ConstrainExtRegion.maxLeft);
+            };
+            yAttr.setter = function(v) {
+                var r = oriYSetter && oriYSetter(v);
+                if (r === undefined) {
+                    r = v;
+                }
+                if (!self.get("constrain")) return r;
+                var _ConstrainExtRegion = _getConstrainRegion.call(
+                    self, self.get("constrain"));
+                return Math.min(Math.max(r,
+                    _ConstrainExtRegion.top),
+                    _ConstrainExtRegion.maxTop);
+            };
+            self.addAttr("x", xAttr);
+            self.addAttr("y", yAttr);
+        },
 
-        },
-        _constrainX:function(ev) {
-            var self=this;
-            var v = ev.newVal,
-                _ConstrainExtRegion = _getConstrainRegion.call(
-                    self, self.get("constrain"));
-            if (!_ConstrainExtRegion) return;
-            if (v >= _ConstrainExtRegion.maxLeft
-                || v <= _ConstrainExtRegion.left) return false;
-        },
-        _constrainY:function(ev) {
-            var self=this;
-            var v = ev.newVal,
-                _ConstrainExtRegion = _getConstrainRegion.call(
-                    self, self.get("constrain"));
-            if (!_ConstrainExtRegion) return;
-            if (v >= _ConstrainExtRegion.maxTop
-                || v <= _ConstrainExtRegion.top) return false;
-        },
         _syncUIConstrain:function() {
             S.log("_syncUIConstrain");
-        },
-
-        _uiSetConstrain:function(v) {
-            S.log("_uiSetConstrain");
-            var self = this;
-            if (v) {
-                self.detach("beforeXChange", self._constrainX, self);
-                self.detach("beforeYChange", self._constrainY, self);
-                self.on("beforeXChange", self._constrainX, self);
-                self.on("beforeYChange", self._constrainY, self);
-            } else {
-                self.detach("beforeXChange", self._constrainX, self);
-                self.detach("beforeYChange", self._constrainY, self);
-            }
         },
         __destructor:function() {
             S.log("constrain-ext __destructor");
