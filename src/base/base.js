8924989caff39d23fa65d29406837becabb83fdd
yiminghe <yiminghe@gmail.com>
2010-11-25 16:55:14 +0800

    test crlf

--- src/base/base.js
+++ src/base/base.js
@@ -1,6 +1,6 @@
 /**
  * @module  Base
- * @author  lifesinger@gmail.com, 承玉<yiminghe@gmail.com>
+ * @author  lifesinger@gmail.com  , 承玉<yiminghe@gmail.com>
  */
 KISSY.add('base', function (S) {
     var UI_SET = "_uiSet",
@@ -268,4 +268,5 @@ KISSY.add('base', function (S) {
 /**
  * 2011-11-08 承玉重构，Base && yui3 Widget 压缩一下，增加扩展类支持，组件初始化生命周期以及 htmlparser
  * 测试 crlf
+ * 
  */
