e7661e5861fd5f0d6e6b18cc7e5588790d785431
lifesinger <lifesinger@gmail.com>
2010-12-13 13:39:11 +0800

    add scope and seed test cases for kslang

--- src/seed/tests/test-es5.js
+++ src/seed/tests/test-es5.js
@@ -1,5 +1,3 @@
-'use strict';
-
 // set host type
 this.hostType = 'console';
 
@@ -11,7 +9,35 @@ load('../lang.js');
 load('kissy-spec.js');
 load('lang-spec.js');
 
+
+//============================>>
+// prepare for scope-spec.js:
+
+function get_script_text(url) {
+    return readFile(url);
+}
+
+function bind_in_scope(obj) {
+    return function(s) {
+        return function() {
+            with (obj) return eval(s);
+        }.call(obj);
+    };
+}
+
+function exec_in_global(url) {
+    load(url);
+}
+
+load('seed-cases.js');
+T.mix(T, {
+    //echo: console.log,
+    run: exec_in_global
+});
+
+load('scope-spec.js');
+//=============================<<
+
 // go
 JasmineLite.excute();
-
 'done';
--- src/seed/tests/test-es5.jsx
+++ src/seed/tests/test-es5.jsx
@@ -15,5 +15,44 @@ $.evalFile(File('../lang.js'));
 $.evalFile(File('kissy-spec.js'));
 $.evalFile(File('lang-spec.js'));
 
+//================================>>
+// prepare for scope-spec.js:
+
+function get_script_text(URI) {
+    var F = File(URI);
+    try {
+        F.open('r');
+        return F.read(F.length);
+    }
+    finally {
+        F.close();
+    }
+}
+
+function bind_in_scope(obj) {
+    return function(s) {
+        return function() {
+            with (obj) return eval(s)
+        }.call(obj)
+    }
+}
+
+exec_in_global = function(host) {
+    var F = bind_in_scope(host);
+    return function(URI) {
+        return F(get_script_text(URI));
+    }
+}(this);
+
+$.evalFile(File('seed-cases.js'));
+T.mix(T, {
+    //echo: console.log,
+    run: exec_in_global
+});
+
+$.evalFile(File('scope-spec.js'));
+//=================================<<
+
+// go
 JasmineLite.excute();
 'done';
