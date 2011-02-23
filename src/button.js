02962a9b035db8f1d62602ab23c8bfd044ca1318
yiminghe <yiminghe@gmail.com>
2011-02-23 12:08:42 +0800

    update btn demo

--- src/button.js
+++ src/button.js
@@ -1,3 +1,7 @@
+/**
+ * simulated button for kissy , inspired by goog button
+ * @author:yiminghe@gmail.com
+ */
 KISSY.add("button", function(S, Button, Render) {
     Button.Render = Render;
     return Button;
