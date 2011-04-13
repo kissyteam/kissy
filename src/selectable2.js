436e12895109a6bdaa6161a8ef6dee6a999471f7
yiminghe <yiminghe@gmail.com>
2011-04-13 13:22:54 +0800

    clean

--- src/selectable2.js
+++
@@ -1,7 +0,0 @@
-KISSY.add("selectable2", function(S, Base, List, Select) {
-    return {
-        Select:Select
-    };
-}, {
-    requires:["selectable2/base","selectable2/list","selectable2/select"]
-});
