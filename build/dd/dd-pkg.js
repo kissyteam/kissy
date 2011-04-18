edc78695f9d259261ca959abb7ca81638922aeb6
yiminghe <yiminghe@gmail.com>
2011-04-18 21:29:27 +0800

    update dd

--- build/dd/dd-pkg.js
+++ build/dd/dd-pkg.js
@@ -804,108 +804,120 @@ KISSY.add("dd/draggable-delegate", function(S, DDM, Draggable, DOM) {
 }, {
     requires:['./ddm','./draggable','dom']
 });/**
- * delegate all draggable nodes to one draggable object
+ * only one droppable instance for multiple droppable nodes
  * @author:yiminghe@gmail.com
  */
-KISSY.add("dd/draggable-delegate", function(S, DDM, Draggable, DOM) {
-    function Delegate() {
-        Delegate.superclass.constructor.apply(this, arguments);
+KISSY.add("dd/droppable-delegate", function(S, DDM, Droppable, DOM, Node) {
+    function DroppableDelegate() {
+        DroppableDelegate.superclass.constructor.apply(this, arguments);
     }
 
-    S.extend(Delegate, Draggable, {
-        _init:function() {
-            var self = this,
-                handlers = self.get('handlers'),
-                node = self.get('container');
-            if (handlers.length == 0) {
-                handlers.push(self.get("selector"));
-            }
-            node.on('mousedown', self._handleMouseDown, self);
-        },
+    S.extend(DroppableDelegate, Droppable, {
 
         /**
-         * 得到适合 handler，从这里开始启动拖放，对于 handlers 选择器字符串数组
-         * @param target
-         */
-        _getHandler:function(target) {
-            var self = this,
-                node = this.get("container"),
-                handlers = self.get('handlers');
-
-            while (target && target[0] !== node[0]) {
-                for (var i = 0; i < handlers.length; i++) {
-                    var h = handlers[i];
-                    if (DOM.test(target[0], h, node[0])) {
-                        return target;
-                    }
-                }
-                target = target.parent();
-            }
-        },
-
-        /**
-         * 找到真正应该移动的节点，对应 selector 属性选择器字符串
-         * @param h
-         */
-        _getNode:function(h) {
-            var node = this.get("container"),sel = this.get("selector");
-            while (h && h[0] != node[0]) {
-                if (DOM.test(h[0], sel, node[0])) {
-                    return h;
-                }
-                h = h.parent();
-            }
-        },
-
-        /**
-         * 父容器监听 mousedown，找到合适的拖动 handlers 以及拖动节点
-         *
+         * 根据鼠标位置得到真正的可放目标，暂时不考虑 mode，只考虑鼠标
          * @param ev
          */
-        _handleMouseDown:function(ev) {
-            var self = this;
-            var target = ev.target;
-            var handler = target && this._getHandler(target);
-            if (!handler) return;
-            var node = this._getNode(handler);
-            if (!node) return;
-            ev.preventDefault();
-            self.set("node", node);
-            self.set("dragNode", node);
-            self._prepare(ev);
+        getNodeFromTarget:function(ev) {
+
+            var pointer = {
+                left:ev.pageX,
+                top:ev.pageY
+            };
+
+            var container = this.get("container"),
+                selector = this.get("selector");
+
+            var allNodes = container.all(selector);
+
+            for (var i = 0; i < allNodes.length; i++) {
+                var n = new Node(allNodes[i]);
+                if (!n.hasClass("ks-dd-proxy") && DDM.inRegion(DDM.region(n), pointer)) {
+                    this.set("lastNode", this.get("node"));
+                    this.set("node", n);
+                    return n;
+                }
+            }
+
+            return null;
+        },
+
+        _handleOut:function() {
+            DroppableDelegate.superclass._handleOut.call(this);
+            this.set("node", null);
+            this.set("lastNode", null);
+        },
+
+        _handleOver:function(ev) {
+            var oldDrop = DDM.get("activeDrop");
+            DDM.set("activeDrop", this);
+            var activeDrag = DDM.get("activeDrag");
+            this.get("node").addClass(DDM.get("prefixCls") + "drop-over");
+            var evt = S.mix({
+                drag:activeDrag,
+                drop:this
+            }, ev);
+            var node = this.get("node"),
+                lastNode = this.get("lastNode");
+
+            if (this != oldDrop
+                || !lastNode
+                || (lastNode && lastNode[0] !== node[0])
+                ) {
+                /**
+                 * 两个可 drop 节点相邻，先通知上次的离开
+                 */
+                if (lastNode) {
+                    this.set("node", lastNode);
+                    DroppableDelegate.superclass._handleOut.call(this);
+                }
+                /**
+                 * 再通知这次的进入
+                 */
+                this.set("node", node);
+                activeDrag.get("node").addClass(DDM.get("prefixCls") + "drag-over");
+                //第一次先触发 dropenter,dragenter
+                activeDrag.fire("dragenter", evt);
+                this.fire("dropenter", evt);
+                DDM.fire("dragenter", evt);
+                DDM.fire("dropenter", evt);
+            } else {
+
+                activeDrag.fire("dragover", evt);
+                this.fire("dropover", evt);
+                DDM.fire("dragover", evt);
+                DDM.fire("dropover", evt);
+            }
         }
     },
     {
         ATTRS:{
             /**
-             * 用于委托的父容器
+             * 上一个成为放目标的节点
+             */
+            lastNode:{
+            }
+            ,
+            /**
+             * 放目标节点选择器
+             */
+            selector:{
+            }
+            ,
+            /**
+             * 放目标所在区域
              */
             container:{
                 setter:function(v) {
                     return S.one(v);
                 }
-            },
-
-            /**
-             * 实际拖放的节点选择器，一般用 tag.cls
-             */
-            selector:{
             }
-
-        /**
-         * 继承来的 handlers : 拖放句柄选择器数组，一般用 [ tag.cls ]
-         * 不设则为 [ selector ]
-         *
-         * handlers:{
-         *  value:[]
-         * }
-         */
         }
     });
 
-    return Delegate;
+    return DroppableDelegate;
 }, {
-    requires:['./ddm','./draggable','dom']
+    requires:['./ddm','./droppable','dom','node']
 });/**
  * auto scroll for drag object's container
  * @author:yiminghe@gmail.com
