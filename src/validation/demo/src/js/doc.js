297ec292d5d60c36ab2edf0da52353e6cfb1380f
satans17 <satans17@gmail.com>
2011-06-27 13:00:21 +0800

    update

---
+++ src/validation/demo/src/js/doc.js
@@ -0,0 +1,7 @@
+		KISSY.config({
+			packages:[{
+					name:"validation",
+					path:"../../"		//Auth存放的路径
+				}
+			]
+		});
---
+++ src/validation/warn/base.js
@@ -0,0 +1,91 @@
+/**
+ * 提示类管理类
+ * @author: 常胤 <lzlu.com>
+ */
+KISSY.add("validation/warn/base", function(S, DOM, Event, Util, BaseClass) {
+
+	var Warn = new function(){
+		
+		var engine = new Util.storage();
+		var style = new Util.storage();
+		
+		/**
+		 * 扩展你的信息提示类
+		 * @param 类名称
+		 * @param 类或对象
+		 */
+		this.extend = function(name,extfun){
+			var newwarn = function(target,config){
+					newwarn.superclass.constructor.call(this, target, config);
+				},
+				ext = S.isFunction(extfun)?extfun():extfun;
+				
+			//保存样式
+			if(ext.style){
+				for(var s in ext.style){
+					this.addStyle(s,S.merge(ext.style[s],{core:name}));
+				}
+				delete ext.style;
+			}
+
+			S.extend(newwarn,BaseClass,ext);
+			//保存类
+			engine.add(name,newwarn);
+			return newwarn;
+		};
+		
+		/**
+		 * 增加内置风格
+		 */
+		this.addStyle = function(name,config){
+			style.add(name,config);
+		};
+		
+		/**
+		 * 获取内置风格
+		 */
+		this.getStyle = function(name){
+			return style.get(name);
+		};
+		
+		/**
+		 * 获取提示类
+		 */
+		this.get = function(name){
+			return engine.get(name);
+		};
+	
+	};
+	
+	
+	return Warn;
+
+}, { requires: ['dom',"event","validation/utils","validation/warn/baseclass"] });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+
