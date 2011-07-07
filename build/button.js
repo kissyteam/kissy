dddfca5eed517d63e5d76541d07152f8808ad13a
yiminghe <yiminghe@gmail.com>
2011-07-07 15:16:17 +0800

    fix button

--- build/button.js
+++ build/button.js
@@ -121,7 +121,7 @@ KISSY.add("button/css3render", function(S, UIBase, ButtonRender) {
         renderUI:function() {
             var self = this,
                 el = self.get("el");
-            el.unselectable().addClass(getCls(self, CLS));
+            el.addClass(getCls(self, CLS)).unselectable();
         },
 
         /**
