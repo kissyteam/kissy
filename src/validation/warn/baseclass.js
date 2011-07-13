df73e2b9a8b5bebfad2d4653372a4c9e61fd3d1f
yiminghe <yiminghe@gmail.com>
2011-07-13 10:06:38 +0800

    fix validation

--- src/validation/warn/baseclass.js
+++ src/validation/warn/baseclass.js
@@ -28,51 +28,51 @@ KISSY.add("validation/warn/baseclass", function(S, DOM, Event) {
 
     S.augment(BaseClass, S.EventTarget, {
 
-            /**
-             * init
-             */
-            init: function() {
-                //dosth
-            },
+        /**
+         * init
+         */
+        init: function() {
+            //dosth
+        },
 
-            /**
-             * 给对象绑定事件
-             *     - checkbox，radiobox默认只能绑定click事件
-             *    - select默认只能绑定select事件
-             *     - 如果你有特殊需求也可以重写此方法
-             * @param {Element} el
-             * @param {String} evttype
-             * @param {Function} fun
-             */
-            _bindEvent: function(el, evttype, fun) {
-                switch (DOM.attr(el, 'type').toLowerCase()) {
-                    case "radio":
-                    case "checkbox":
-                        Event.on(el, 'click', fun);
-                        break;
-                    case "select":
-                    case "select-multi":
-                    case "file":
-                        Event.on(el, "change", fun);
-                        break;
-                    default:
-                        Event.on(el, evttype, fun);
-                }
-            },
-
-            /**
-             * 显示出错信息
-             * @param {Boolean} result
-             * @param {String} msg
-             * @param  evttype
-             */
-            showMessage: function(result, msg, evttype) {
-                result = 1;
-                msg = 1;
-                evttype = 1;
+        /**
+         * 给对象绑定事件
+         *     - checkbox，radiobox默认只能绑定click事件
+         *    - select默认只能绑定select事件
+         *     - 如果你有特殊需求也可以重写此方法
+         * @param {Element} el
+         * @param {String} evttype
+         * @param {Function} fun
+         */
+        _bindEvent: function(el, evttype, fun) {
+            switch ((DOM.attr(el, 'type') || "input").toLowerCase()) {
+                case "radio":
+                case "checkbox":
+                    Event.on(el, 'click', fun);
+                    break;
+                case "select":
+                case "select-multi":
+                case "file":
+                    Event.on(el, "change", fun);
+                    break;
+                default:
+                    Event.on(el, evttype, fun);
             }
+        },
 
-        });
+        /**
+         * 显示出错信息
+         * @param {Boolean} result
+         * @param {String} msg
+         * @param  evttype
+         */
+        showMessage: function(result, msg, evttype) {
+            result = 1;
+            msg = 1;
+            evttype = 1;
+        }
+
+    });
 
     return BaseClass;
 
