8d9dc6c9aea1acec30b33d4b2a8e81cf0551cef6
yiminghe <yiminghe@gmail.com>
2011-08-01 11:48:57 +0800

    fix slide on pauseOnHover

--- build/switchable.js
+++ build/switchable.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2011, KISSY UI Library v1.20dev
 MIT Licensed
-build time: Jul 28 15:35
+build time: Aug 1 11:46
 */
 /**
  * Switchable
@@ -997,10 +997,10 @@ KISSY.add('switchable/autoplay', function(S, Event, Switchable, undefined) {
      * 添加默认配置
      */
     S.mix(Switchable.Config, {
-            autoplay: false,
-            interval: 5, // 自动播放间隔时间
-            pauseOnHover: true  // triggerType 为 mouse 时，鼠标悬停在 slide 上是否暂停自动播放
-        });
+        autoplay: false,
+        interval: 5, // 自动播放间隔时间
+        pauseOnHover: true  // triggerType 为 mouse 时，鼠标悬停在 slide 上是否暂停自动播放
+    });
 
     /**
      * 添加插件
@@ -1009,56 +1009,56 @@ KISSY.add('switchable/autoplay', function(S, Event, Switchable, undefined) {
      */
     Switchable.Plugins.push({
 
-            name: 'autoplay',
+        name: 'autoplay',
 
-            init: function(host) {
+        init: function(host) {
 
-                var cfg = host.config,
-                    interval = cfg.interval * 1000,
-                    timer;
+            var cfg = host.config,
+                interval = cfg.interval * 1000,
+                timer;
 
-                if (!cfg.autoplay) return;
+            if (!cfg.autoplay) return;
 
-                // 鼠标悬停，停止自动播放
-                if (cfg.pauseOnHover) {
-                    Event.on(host.container, 'mouseenter', host.stop, host);
-                    Event.on(host.container, 'mouseleave', host.start, host);
-                }
-
-                function startAutoplay() {
-                    // 设置自动播放
-                    timer = S.later(function() {
-                        if (host.paused) return;
-                        // 自动播放默认 forward（不提供配置），这样可以保证 circular 在临界点正确切换
-                        host.switchTo(host.activeIndex < host.length - 1 ?
-                            host.activeIndex + 1 : 0,
-                            'forward');
-                    }, interval, true);
-                }
-
-                // go
-                startAutoplay();
-
-                // 添加 stop 方法，使得外部可以停止自动播放
-                host.stop = function() {
-                    if (timer) {
-                        timer.cancel();
-                        timer = undefined;
-                    }
-                    // paused 可以让外部知道 autoplay 的当前状态
-                    host.paused = true;
-                };
-
-                host.start = function() {
-                    if (timer) {
-                        timer.cancel();
-                        timer = undefined;
-                    }
-                    host.paused = false;
-                    startAutoplay();
-                };
+            function startAutoplay() {
+                // 设置自动播放
+                timer = S.later(function() {
+                    if (host.paused) return;
+                    // 自动播放默认 forward（不提供配置），这样可以保证 circular 在临界点正确切换
+                    host.switchTo(host.activeIndex < host.length - 1 ?
+                        host.activeIndex + 1 : 0,
+                        'forward');
+                }, interval, true);
             }
-        });
+
+            // go
+            startAutoplay();
+
+            // 添加 stop 方法，使得外部可以停止自动播放
+            host.stop = function() {
+                if (timer) {
+                    timer.cancel();
+                    timer = undefined;
+                }
+                // paused 可以让外部知道 autoplay 的当前状态
+                host.paused = true;
+            };
+
+            host.start = function() {
+                if (timer) {
+                    timer.cancel();
+                    timer = undefined;
+                }
+                host.paused = false;
+                startAutoplay();
+            };
+
+            // 鼠标悬停，停止自动播放
+            if (cfg.pauseOnHover) {
+                Event.on(host.container, 'mouseenter', host.stop, host);
+                Event.on(host.container, 'mouseleave', host.start, host);
+            }
+        }
+    });
     return Switchable;
 }, { requires:["event","./base"]});
 /**
