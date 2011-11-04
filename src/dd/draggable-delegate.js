7aecd7dcc9aa3bb98ccd859d7305983eff2c38e2
yiminghe <yiminghe@gmail.com>
2011-11-04 11:58:41 +0800

    adjust draggable-delegate

--- src/dd/draggable-delegate.js
+++ src/dd/draggable-delegate.js
@@ -2,113 +2,117 @@
  * delegate all draggable nodes to one draggable object
  * @author yiminghe@gmail.com
  */
-KISSY.add("dd/draggable-delegate", function(S, DDM, Draggable, DOM,Node) {
+KISSY.add("dd/draggable-delegate", function(S, DDM, Draggable, DOM, Node) {
     function Delegate() {
         Delegate.superclass.constructor.apply(this, arguments);
     }
 
     S.extend(Delegate, Draggable, {
-        _init:function() {
-            var self = this,
-                handlers = self.get('handlers'),
-                node = self.get('container');
-            if (handlers.length === 0) {
-                handlers.push(self.get("selector"));
-            }
-            node.on('mousedown', self._handleMouseDown, self);
-        },
-
-        /**
-         * 得到适合 handler，从这里开始启动拖放，对于 handlers 选择器字符串数组
-         * @param target
-         */
-        _getHandler:function(target) {
-            var self = this,
-                node = this.get("container"),
-                handlers = self.get('handlers');
-
-            while (target && target[0] !== node[0]) {
-                for (var i = 0; i < handlers.length; i++) {
-                    var h = handlers[i];
-                    if (DOM.test(target[0], h, node[0])) {
-                        return target;
-                    }
+            _init:function() {
+                var self = this,
+                    handlers = self.get('handlers'),
+                    node = self.get('container');
+                if (handlers.length === 0) {
+                    handlers.push(self.get("selector"));
                 }
-                target = target.parent();
-            }
-        },
+                node.on('mousedown', self._handleMouseDown, self);
+            },
 
-        /**
-         * 找到真正应该移动的节点，对应 selector 属性选择器字符串
-         * @param h
-         */
-        _getNode:function(h) {
-            var node = this.get("container"),sel = this.get("selector");
-            while (h && h[0] != node[0]) {
-                if (DOM.test(h[0], sel, node[0])) {
-                    return h;
-                }
-                h = h.parent();
-            }
-        },
-
-        /**
-         * 父容器监听 mousedown，找到合适的拖动 handlers 以及拖动节点
-         *
-         * @param ev
-         */
-        _handleMouseDown:function(ev) {
-            var self = this;
-            var target = new Node(ev.target);
-            var handler = target && this._getHandler(target);
-            if (!handler) {
-                return;
-            }
-            var node = this._getNode(handler);
-            if (!node) {
-                return;
-            }
-            ev.preventDefault();
-            self.set("node", node);
-            self.set("dragNode", node);
-            self._prepare(ev);
-        },
-
-        destroy:function() {
-            var self = this;
-            self.get("container").detach('mousedown',
-                self._handleMouseDown,
-                self);
-            self.detach();
-        }
-    },
-    {
-        ATTRS:{
             /**
-             * 用于委托的父容器
+             * 得到适合 handler，从这里开始启动拖放，对于 handlers 选择器字符串数组
+             * @param target
              */
-            container:{
-                setter:function(v) {
-                    return Node.one(v);
+            _getHandler:function(target) {
+                var self = this,
+                    node = this.get("container"),
+                    handlers = self.get('handlers');
+
+                while (target && target[0] !== node[0]) {
+                    for (var i = 0; i < handlers.length; i++) {
+                        var h = handlers[i];
+                        if (DOM.test(target[0], h, node[0])) {
+                            return target;
+                        }
+                    }
+                    target = target.parent();
                 }
             },
 
             /**
-             * 实际拖放的节点选择器，一般用 tag.cls
+             * 找到真正应该移动的节点，对应 selector 属性选择器字符串
+             * @param h
              */
-            selector:{
-            }
+            _getNode:function(h) {
+                var node = this.get("container"),sel = this.get("selector");
+                while (h && h[0] != node[0]) {
+                    if (DOM.test(h[0], sel, node[0])) {
+                        return h;
+                    }
+                    h = h.parent();
+                }
+            },
 
-        /**
-         * 继承来的 handlers : 拖放句柄选择器数组，一般用 [ tag.cls ]
-         * 不设则为 [ selector ]
-         *
-         * handlers:{
-         *  value:[]
-         * }
-         */
-        }
-    });
+            /**
+             * 父容器监听 mousedown，找到合适的拖动 handlers 以及拖动节点
+             *
+             * @param ev
+             */
+            _handleMouseDown:function(ev) {
+                var self = this;
+                var target = new Node(ev.target);
+                var handler = target && this._getHandler(target);
+                if (!handler) {
+                    return;
+                }
+                var node = this._getNode(handler);
+                if (!node) {
+                    return;
+                }
+                ev.preventDefault();
+                self.set("node", node);
+                self.set("dragNode", node);
+                self._prepare(ev);
+            },
+
+            destroy:function() {
+                var self = this;
+                self.get("container").detach('mousedown',
+                    self._handleMouseDown,
+                    self);
+                self.detach();
+            }
+        },
+        {
+            ATTRS:{
+                /**
+                 * 用于委托的父容器
+                 */
+                container:{
+                    setter:function(v) {
+                        return Node.one(v);
+                    }
+                },
+
+                /**
+                 * 实际拖放的节点选择器，一般用 tag.cls
+                 */
+                selector:{
+                },
+
+                /**
+                 * 继承来的 handlers : 拖放句柄选择器数组，一般用 [ tag.cls ]
+                 * 不设则为 [ selector ]
+                 **/
+                handlers:{
+                    value:[],
+                    // 覆盖父类的 getter ，这里不返回节点
+                    getter:function(vs) {
+                        return vs;
+                    }
+                }
+
+            }
+        });
 
     return Delegate;
 }, {
