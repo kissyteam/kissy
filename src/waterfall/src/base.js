d05dbc3deb270a70c9f2cfdbb07f1a69a088bb19
yiminghe <yiminghe@gmail.com>
2012-02-13 14:42:11 +0800

    fix #99

--- src/waterfall/src/base.js
+++ src/waterfall/src/base.js
@@ -85,14 +85,19 @@ KISSY.add("waterfall/base", function (S, Node, Base) {
             }
         },
 
-        colWidth:{}
+        colWidth:{},
+
+        colItems:{
+            value:[]
+        }
     };
 
     function doResize() {
         var self = this,
             containerRegion = self._containerRegion;
         // 宽度没变就没必要调整
-        if (containerRegion && self.get("container").width() === containerRegion.width) {
+        if (containerRegion &&
+            self.get("container").width() === containerRegion.width) {
             return
         }
         self.adjust();
@@ -111,6 +116,7 @@ KISSY.add("waterfall/base", function (S, Node, Base) {
         S.each(curColHeights, function (v, i) {
             curColHeights[i] = 0;
         });
+        self.set("colItems", []);
     }
 
     function adjustItem(itemRaw) {
@@ -135,7 +141,7 @@ KISSY.add("waterfall/base", function (S, Node, Base) {
         // 元素保持间隔不变，居中
         var margin = Math.max(containerRegion.width - curColCount * self.get("colWidth"), 0) / 2;
         item.css({
-            //left:dest * Math.max(containerRegion.width / curColCount, self.get("colWidth"))
+            // left:dest * Math.max(containerRegion.width / curColCount, self.get("colWidth"))
             //    + containerRegion.left,
             // 元素间固定间隔好点
             left:dest * self.get("colWidth") + margin,
@@ -149,7 +155,33 @@ KISSY.add("waterfall/base", function (S, Node, Base) {
             container.append(item);
         }
         curColHeights[dest] += item.outerHeight(true);
+        var colItems = self.get("colItems");
+        colItems[dest] = colItems[dest] || [];
+        colItems[dest].push(item);
+        item.attr("data-waterfall-col", dest);
         return item;
+    }
+
+    function addItem(itemRaw) {
+        var self = this,
+            curColHeights = self.get("curColHeights"),
+            container = self.get("container"),
+            item = adjustItem.call(self, itemRaw),
+            effect = self.get("effect");
+        if (!effect.effect ||
+            effect.effect !== "fadeIn") {
+            return;
+        }
+        // only allow fadeIn temporary
+        item.animate({
+            opacity:1
+        }, {
+            duration:effect.duration,
+            easing:effect.easing,
+            complete:function () {
+                item.css("opacity", "");
+            }
+        });
     }
 
     S.extend(Waterfall, Base,
@@ -157,91 +189,148 @@ KISSY.add("waterfall/base", function (S, Node, Base) {
          * @lends Waterfall
          */
         {
-        isAdjusting:function () {
-            return !!this._adjuster;
-        },
-        _init:function () {
-            var self = this;
-            // 一开始就 adjust 一次，可以对已有静态数据处理
-            doResize.call(self);
-            self.__onResize = S.buffer(doResize, RESIZE_DURATION, self);
-            $(window).on("resize", self.__onResize);
-        },
+            isAdjusting:function () {
+                return !!this._adjuster;
+            },
 
-        /**
-         * 调整所有的元素位置
-         * @param callback
-         */
-        adjust:function (callback) {
-            S.log("waterfall:adjust");
-            var self = this,
-                items = self.get("container").all(".ks-waterfall");
-            /* 正在加，直接开始这次调整，剩余的加和正在调整的一起处理 */
-            /* 正在调整中，取消上次调整，开始这次调整 */
-            if (self.isAdjusting()) {
-                self._adjuster.stop();
-            }
-            /*计算容器宽度等信息*/
-            recalculate.call(self);
-            return self._adjuster = timedChunk(items, self._addItem, self, function () {
-                self.get("container").height(Math.max.apply(Math, self.get("curColHeights")));
-                self._adjuster = 0;
-                callback && callback.call(self);
+            _init:function () {
+                var self = this;
+                // 一开始就 adjust 一次，可以对已有静态数据处理
+                doResize.call(self);
+                self.__onResize = S.buffer(doResize, RESIZE_DURATION, self);
+                $(window).on("resize", self.__onResize);
+            },
 
-                items.length && self.fire('adjustComplete', {
-                    items:items
+            adjustItem:function (item, cfg) {
+                var self = this;
+
+                if (self.isAdjusting()) {
+                    return;
+                }
+
+                var originalOuterHeight = item.outerHeight(true),
+                    outerHeight,
+                    remove = false;
+                if (cfg.process) {
+                    remove = cfg.process.call(self);
+                }
+                if (remove) {
+                    outerHeight = 0;
+                } else {
+                    outerHeight = item.outerHeight(true);
+                }
+                var diff = outerHeight - originalOuterHeight,
+                    curColHeights = self.get("curColHeights"),
+                    dest = parseInt(item.attr("data-waterfall-col")),
+                    colItems = self.get("colItems")[dest],
+                    items = [],
+                    original = Math.max.apply(Math, curColHeights),
+                    now;
+
+                for (var i = 0; i < colItems.length; i++) {
+                    if (colItems[i][0] === item[0]) {
+                        break;
+                    }
+                }
+
+                i++;
+
+                while (i < colItems.length) {
+                    items.push(colItems[i]);
+                    i++;
+                }
+
+                curColHeights[dest] += diff;
+
+                now = Math.max.apply(Math, curColHeights);
+
+                if (now != original) {
+                    self.get("container").height(now);
+                }
+
+                return self._adjuster = timedChunk(items, function (item) {
+                    item.css("top", parseInt(item.css("top")) + diff);
+                }, null, function () {
+                    self._adjuster = 0;
+                    cfg && cfg.callback && cfg.callback.call(self);
                 });
-            });
-        },
+            },
 
-        addItems:function (items, callback) {
-            var self = this;
+            removeItem:function (item, cfg) {
+                var self = this;
+                self.adjustItem(item, {
+                    process:function () {
+                        item.remove();
+                        return true;
+                    },
+                    callback:function () {
+                        var dest = parseInt(item.attr("data-waterfall-col")),
+                            colItems = self.get("colItems")[dest];
+                        for (var i = 0; i < colItems.length; i++) {
+                            if (colItems[i][0] == item[0]) {
+                                colItems.splice(i, 1);
+                                break;
+                            }
+                        }
+                        if (cfg && cfg.callback) {
+                            cfg.callback.call(self);
+                        }
+                    }
+                });
+            },
 
-            /* 正在调整中，直接这次加，和调整的节点一起处理 */
-            /* 正在加，直接这次加，一起处理 */
-            self._adder = timedChunk(items,
-                self._addItem,
-                self,
-                function () {
-                    self.get("container").height(Math.max.apply(Math,
-                        self.get("curColHeights")));
-                    self._adder = 0;
+            /**
+             * 调整所有的元素位置
+             * @param [callback]
+             */
+            adjust:function (callback) {
+                S.log("waterfall:adjust");
+                var self = this,
+                    items = self.get("container").all(".ks-waterfall");
+                /* 正在加，直接开始这次调整，剩余的加和正在调整的一起处理 */
+                /* 正在调整中，取消上次调整，开始这次调整 */
+                if (self.isAdjusting()) {
+                    self._adjuster.stop();
+                }
+                /*计算容器宽度等信息*/
+                recalculate.call(self);
+                return self._adjuster = timedChunk(items, addItem, self, function () {
+                    self.get("container").height(Math.max.apply(Math, self.get("curColHeights")));
+                    self._adjuster = 0;
                     callback && callback.call(self);
 
-                    items.length && self.fire('addComplete', {
+                    items.length && self.fire('adjustComplete', {
                         items:items
                     });
                 });
+            },
 
-            return self._adder;
-        },
+            addItems:function (items, callback) {
+                var self = this;
 
-        _addItem:function (itemRaw) {
-            var self = this,
-                curColHeights = self.get("curColHeights"),
-                container = self.get("container"),
-                item = adjustItem.call(self, itemRaw),
-                effect = self.get("effect");
-            if (!effect.effect ||
-                effect.effect !== "fadeIn") {
-                return;
+                /* 正在调整中，直接这次加，和调整的节点一起处理 */
+                /* 正在加，直接这次加，一起处理 */
+                self._adder = timedChunk(items,
+                    addItem,
+                    self,
+                    function () {
+                        self.get("container").height(Math.max.apply(Math,
+                            self.get("curColHeights")));
+                        self._adder = 0;
+                        callback && callback.call(self);
+
+                        items.length && self.fire('addComplete', {
+                            items:items
+                        });
+                    });
+
+                return self._adder;
+            },
+
+            destroy:function () {
+                $(window).detach("resize", this.__onResize);
             }
-            // only allow fadeIn temporary
-            item.animate({
-                opacity:1
-            }, {
-                duration:effect.duration,
-                easing:effect.easing,
-                complete:function () {
-                    item.css("opacity", "");
-                }
-            });
-        },
-
-        destroy:function () {
-            $(window).detach("resize", this.__onResize);
-        }
-    });
+        });
 
 
     return Waterfall;
