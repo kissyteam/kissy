b4d6c787a6fc22a426d79001ae12f8235e48c397
yiminghe <yiminghe@gmail.com>
2012-05-31 23:30:27 +0800

    update docs

--- src/seed/src/loader/configs.js
+++ src/seed/src/loader/configs.js
@@ -7,31 +7,28 @@
         return;
     }
     var utils = S.Loader.Utils;
-    /**
-     * modify current module path
-     * @private
-     * @param rules
-     * @example
-     * <code>
-     *      [
-     *          [/(.+-)min(.js(\?t=\d+)?)$/,"$1$2"],
-     *          [/(.+-)min(.js(\?t=\d+)?)$/,function(_,m1,m2){
-     *              return m1+m2;
-     *          }]
-     *      ]
-     * </code>
+    /*
+      modify current module path
+      <code>
+           [
+               [/(.+-)min(.js(\?t=\d+)?)$/,"$1$2"],
+               [/(.+-)min(.js(\?t=\d+)?)$/,function(_,m1,m2){
+                   return m1+m2;
+               }]
+           ]
+      </code>
      */
     S.configs.map = function (rules) {
         var self = this;
         return self.Config.mappedRules = (self.Config.mappedRules || []).concat(rules || []);
     };
 
-    /**
-     * 包声明
-     * biz -> .
-     * 表示遇到 biz/x
-     * 在当前网页路径找 biz/x.js
-     * @private
+    /*
+      包声明
+      biz -> .
+      表示遇到 biz/x
+      在当前网页路径找 biz/x.js
+      @private
      */
     S.configs.packages = function (cfgs) {
         var self = this,
@@ -59,30 +56,30 @@
 
     /*
      只用来指定模块依赖信息.
-     * <code>
-     *
-     * KISSY.config({
-     *  base:'',
-     *  // dom-min.js
-     *  debug:'',
-     *  combine:true,
-     *  tag:'',
-     *  packages:{
-     *      "biz1": {
-     *          // path change to base
-     *          base: "haha",
-     *          // x.js
-     *          debug:'',
-     *          tag:'',
-     *          combine:false,
-     *      }
-     *  },
-     *  modules:{
-     *      "biz1/main" : {
-     *          requires: [ "biz1/part1" , "biz1/part2" ]
-     *      }
-     *  }
-     * });
+      <code>
+
+      KISSY.config({
+       base:'',
+       // dom-min.js
+       debug:'',
+       combine:true,
+       tag:'',
+       packages:{
+           "biz1": {
+               // path change to base
+               base: "haha",
+               // x.js
+               debug:'',
+               tag:'',
+               combine:false,
+           }
+       },
+       modules:{
+           "biz1/main" : {
+               requires: [ "biz1/part1" , "biz1/part2" ]
+           }
+       }
+      });
      */
     S.configs.modules = function (modules) {
         var self = this;
@@ -97,8 +94,8 @@
 
     S.configs.modules.order = 10;
 
-    /**
-     * KISSY 's base path.
+    /*
+      KISSY 's base path.
      */
     S.configs.base = function (base) {
         var self = this;
