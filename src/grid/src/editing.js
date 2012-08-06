a846263f7a42bbe1c06269917d18c9afbb934b75
dxq <Administrator@WIN-8LV4I7FOC2M.(none)>
2012-08-06 22:35:42 +0800

    update grid editor

---
+++ src/grid/src/editing.js
@@ -0,0 +1,308 @@
+KISSY.add('grid/editing',function(S,Component,EditorPanel){
+	
+	var DOM = S.DOM,
+		Event = S.Event,
+		doc = document;
+	/**
+	* @name Grid.Plugins.Editing
+    * @constructor
+	*/
+	var editing = function(config){
+		editing.superclass.constructor.call(this, config);
+	}
+	
+	S.extend(editing,S.Base);
+	
+	editing.ATTRS = 
+	 /**
+	 * @lends Grid.Plugins.Editing.prototype
+	 */
+	{
+		/**
+		* @private
+		*/
+		currentEditorPanel : {
+			value : null
+		},
+		/**
+		* @private
+		*/
+		currentCell : {
+		
+		},
+		/**
+		* @private
+		*/
+		editorPanels : {
+			value : []
+		},
+		/**
+		* @private
+		*/
+		grid : {
+		
+		},
+		/**
+		* The event which triggers editing. Maybe one of:
+		* <ul>
+		* <li>cellclick</li>
+		* <li>celldblclick</li>
+		* <li>cellfocus</li>
+		* <li>rowfocus</li>
+		* </ul>
+		* @type {String}
+		* @default 'cellclick'
+		*/
+		triggerEvent : {
+			value : 'cellclick'
+		}
+	};
+	
+	S.augment(editing,
+	/**
+	 * @lends Grid.Plugins.Editing.prototype
+	 */
+	{
+		/**
+		* @private
+		*/
+		renderUI : function(grid){
+			var _self = this,
+				columns = grid.get('columns'),
+				editors = [];
+			_self.set('grid',grid);	
+			_self._initEditors(columns);
+		},
+		/**
+		* @private
+		*/
+		bindUI : function(grid){
+			var _self = this,
+				triggerEvent = _self.get('triggerEvent'),
+				editorPanels = _self.get('editorPanels');
+			S.each(editorPanels,function(editorPanel){
+				_self._initPanelEvent(editorPanel);
+			});
+			
+			grid.on(triggerEvent,function(e){
+				_self.showEitor(e.field,e.record,e.cell,e.row);
+			});
+			Event.on(doc,'click',function(e){
+				var dom = e.target,
+					curPanel = _self.get('currentEditorPanel'),
+					curCell = _self.get('currentCell');
+				if(curPanel && curCell){
+					if(!(curPanel.containsElement(dom) || curCell == dom || S.one(curCell).contains(dom)) && curPanel.get('visible')){
+						curPanel.hide();
+						_self.set('currentEditorPanel',null);
+						_self.set('currentCell',null);
+					}
+				}
+			});
+			
+		},
+		/**
+		* @private
+		*/
+		cancelEdit : function(){},
+		/**
+		* When user trigger the event so as 'cellclick',show the editor
+		*/
+		showEitor : function(field,record,cell,row){
+			var _self = this,
+				editorPanel = _self._findEditorPanel(field),
+				alignNode = _self._getAlignNode(cell,row);
+			if(editorPanel){
+				_self.set('currentEditorPanel',editorPanel);
+				_self.set('currentCell',cell);
+				editorPanel.set('record',record);
+				editorPanel.set('align',{
+					node : alignNode,
+					points : ['tl','tl']
+				});
+				_self._beforeShowEditor(editorPanel,cell);
+				editorPanel.show();
+				editorPanel.focus();
+				
+			}
+		},
+		/**
+		* before editor panel showing,subclass can do some action
+		* @protect 
+		*/
+		_beforeShowEditor : function(editorPanel){
+			
+		},
+		/**
+		* @protect 
+		*/
+		_findEditorPanel : function(field){
+			var _self = this,
+				editorPanels = _self.get('editorPanels');
+			//默认取第一个编辑器，行编辑状态下就使用唯一的编辑器容器
+			return editorPanels[0];
+		},
+		/**
+		* @protect 
+		*/
+		_getAlignNode : function(cell,row){
+			return row;
+		},
+		/**
+		* @protect 
+		*/
+		_initEditors : function(columns){
+			
+		},
+		/**
+		* @protect
+		*/
+		_initPanelEvent : function(editorPanel){
+
+		},
+		destroy : function(){
+			var _self = this,
+				editorPanels = _self.get('editorPanels');
+			S.each(editorPanels,function(editorPanel){
+				editorPanel.destroy();
+			});
+		}
+	});
+	
+	/**
+	* The plugin of grid which is used to edit the grid cell.
+	* @name Grid.Plugins.CellEditing
+    * @constructor
+	*/
+	var cellEditing = function(config){
+		cellEditing.superclass.constructor.call(this, config);
+	}
+	
+	S.extend(cellEditing,editing);
+	
+	S.augment(cellEditing,{
+	
+		/**
+		* before editor panel showing,subclass can do some action
+		* @protect 
+		*/
+		_beforeShowEditor : function(editorPanel,cell){
+			var editor = editorPanel.get('children')[0];
+				
+			editor.set('width',DOM.width(cell));
+		},
+		/**
+		* @protect 
+		*/
+		_getAlignNode : function(cell,row){
+			return cell;
+		},
+		/**
+		* @protect 
+		*/
+		_findEditorPanel : function(field){
+			var _self = this,
+				editorPanels = _self.get('editorPanels'),
+				result = null;
+			S.each(editorPanels,function(editorPanel){
+				var editor = editorPanel.get('children')[0];
+				if(editor.get('field') === field){
+					result = editorPanel;
+					return false;
+				}
+			});
+			
+			return result;
+		},
+		/**
+		* @protect 
+		*/
+		_initEditors : function(columns){
+			var _self = this,
+				editorPanels = _self.get('editorPanels');
+			S.each(columns,function(column){
+				var editor = column.get('editor');
+				if(editor){
+					if(!(editor instanceof Component.Controller)){
+						editor.field = column.get('dataIndex');
+					}
+					var panel = new EditorPanel({
+						children : [editor]
+					});
+					panel.render();
+					editorPanels.push(panel);
+				}
+			});
+		},
+		/**
+		* @protect
+		*/
+		_initPanelEvent : function(editorPanel){
+			var _self = this,
+				grid = _self.get('grid'),
+				store = grid.get('store');
+			editorPanel.on('changed',function(e){
+				var editor = e.target,
+					record = editor.get('record'),
+					field = editor.get('field'),
+					val = editor.getValue();
+				store.setValue(record,field,val);
+			});
+		}
+	});
+	
+	/**
+	* The plugin of grid which is used to edit the grid row.
+	* @name Grid.Plugins.CellEditing
+    * @constructor
+	*/
+	var rowEditing = function(config){
+		rowEditing.superclass.constructor.call(this, config);
+	}
+	
+	S.extend(rowEditing,editing);
+	
+	S.augment(rowEditing,{
+		/**
+		* before editor panel showing,subclass can do some action
+		* @protect 
+		*/
+		_beforeShowEditor : function(editorPanel,cell){
+			var editors = editorPanel.get('children');
+				
+			S.each(editors,function(editor){
+				editor.set('width',DOM.width(cell));
+			});
+			
+		},
+		/**
+		* @protect 
+		*/
+		_initEditors : function(columns){
+			var _self = this,
+				editorPanels = _self.get('editorPanels'),
+				editorPanel = null,
+				editors = [];
+			//行编辑模式下，所有的编辑器共用同一个容器
+			S.each(columns,function(column){
+				var editor = column.get('editor');
+				if(editor){
+					if(!(editor instanceof Component.Controller)){
+						editor.field = column.get('dataIndex');
+					}
+					editors.push(panel);
+				}else{//如果不存在则显示文本
+					
+				}
+			});
+			
+		}
+	});
+	
+	editing.CellEditing = cellEditing;
+	editing.RowEditing = rowEditing;
+	return editing;
+	
+},{
+    requires:['component','grid/editorpanel']
+});
---
+++ src/grid/src/editorpanel.js
@@ -0,0 +1,136 @@
+/**
+ * @fileOverview This class specifies the definition for a container of grid editor.
+ * @author dxq613@gmail.com
+ */
+KISSY.add('grid/editorpanel', function (S, Component) {
+
+	var CLS_EDITOR_CONTAINER = 'ks-grid-editor-container';
+	
+	var editorPanelRender = Component.Render.extend([
+		Component.UIBase.Position.Render,
+		Component.UIBase.Align.Render
+	],{
+	
+		/**
+		* @private
+		*/
+		renderUI:function () {
+			var _self = this,
+				el = _self.get('el'),
+				tpl = _self.get('tpl');
+			new S.Node(tpl).appendTo(el);
+		},
+		/**
+         * @see {Component.Render#getContentElement}
+         */
+        getContentElement:function () {
+			return this.get('el').one('.' + CLS_EDITOR_CONTAINER);	
+		}
+	},{
+		ATTRS : {
+			visibleMode : {
+				value : 'visibility'
+			}
+		}
+	});
+	/**
+     * This is a base class of editor container.
+	 * In general, this class will not be instanced directly.
+	 * @private
+     * @name Grid.EditorPanel
+     * @constructor
+     * @extends Component.Controller
+	 * @extends Component.UIBase.Align
+     */
+	var editorPanel = Component.Controller.extend([
+			Component.UIBase.Position,
+			Component.UIBase.Align
+		],{
+		
+		/**
+		* identify all children contains this element
+		* @param {HTMLElement|Node} element one Node or dom element
+		* @return whether the element is in the editor's of this panel
+		*/
+		containsElement : function(element){
+			var _self = this,
+				children = _self.get('children'),
+				result = false;
+			S.each(children,function(editor){
+				if(editor.containsElement(element)){
+					result = true;
+					return false;
+				}
+			});
+			
+			return result;
+		},
+		/**
+		* make the first editor focused
+		*/
+		focus : function(){
+			var _self = this,
+				children = _self.get('children'),
+				firstEditor = children[0];
+			if(firstEditor){
+				firstEditor.focus();
+			}
+		},
+		//set the editing record
+		_uiSetRecord : function(v){
+			var _self = this,
+				children = _self.get('children');
+			S.each(children,function(editor){
+				var field = editor.get('field');
+				if(field){
+					editor.set('record',v);
+				}
+			});
+		}
+	},
+	{
+		ATTRS : {
+			/**
+			* @private 
+			* @override
+			*/	
+			focusable:{
+				view : true,
+				value : false
+			},
+			/**
+			* The editing record
+			*/
+			record : {
+				
+			},
+			/**
+			 * An template used to create the internal structure inside this Component's encapsulating Element.
+			 * User can use the syntax of KISSY's template component.
+			 * Only in the configuration of the editor container.
+			 * @type {String}
+			 * <pre>
+			 *    
+			 * </pre>
+			 */
+			tpl : {
+				view : true,
+				value : '<div class="' + CLS_EDITOR_CONTAINER + '"></div>'
+			},
+			/**
+			 * @private
+			 */
+			xrender:{
+				value : editorPanelRender
+			}
+		}
+	}, 
+	{
+            xclass:'grid-editor-panel',
+            priority:1
+    });
+	
+	return editorPanel;
+},{
+    requires:['component']
+});
