0094c93a4e2323fbd79442401b9eb6b84403e767
yiminghe <yiminghe@gmail.com>
2012-12-11 00:46:55 +0800

    rebuild

--- build/kissy-nodejs.js
+++ build/kissy-nodejs.js
@@ -1,7 +1,7 @@
 ﻿/*
 Copyright 2012, KISSY UI Library v1.40dev
 MIT Licensed
-build time: Dec 11 00:01
+build time: Dec 11 00:46
 */
 /**
  * @ignore
@@ -39,11 +39,11 @@ var KISSY = (function (undefined) {
 
         /**
          * The build time of the library.
-         * NOTICE: '20121211000137' will replace with current timestamp when compressing.
+         * NOTICE: '20121211004640' will replace with current timestamp when compressing.
          * @private
          * @type {String}
          */
-        __BUILD_TIME: '20121211000137',
+        __BUILD_TIME: '20121211004640',
         /**
          * KISSY Environment.
          * @private
@@ -2047,6 +2047,7 @@ var KISSY = (function (undefined) {
 
     KISSY.Defer = Defer;
     KISSY.Promise = Promise;
+    Promise.Defer = Defer;
 
     S.mix(Promise,
         /**
@@ -5146,7 +5147,7 @@ var KISSY = (function (undefined) {
             // file limit number for a single combo url
             comboMaxFileNum: 40,
             charset: 'utf-8',
-            tag: '20121211000137'
+            tag: '20121211004640'
         }, getBaseInfo()));
     }
 
