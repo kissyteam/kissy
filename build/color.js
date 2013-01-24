3ee519f250387a4ed5dceab5338714f3915e09ff
tangoboy <zhulutangoboy@gmail.com>
2013-01-24 19:37:44 +0800

    make color parse rgb(1,1,1). Fixes #292

--- build/color.js
+++ build/color.js
@@ -1,7 +1,7 @@
 ﻿/*
-Copyright 2012, KISSY UI Library v1.40dev
+Copyright 2013, KISSY UI Library v1.40dev
 MIT Licensed
-build time: Dec 20 22:23
+build time: Jan 24 20:42
 */
 /**
  * Color For KISSY.
@@ -10,7 +10,7 @@ build time: Dec 20 22:23
  */
 KISSY.add("color", function (S, Base) {
 
-    var rgbaRe = /\s*rgba?\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*,(?:,\s*(\d+))?\)\s*/,
+    var rgbaRe = /\s*rgba?\s*\(\s*([0-9]+)\s*,\s*([0-9]+)\s*,\s*([0-9]+)\s*(?:,\s*(\d+))?\)\s*/,
         hexRe = /\s*#([0-9a-fA-F][0-9a-fA-F]?)([0-9a-fA-F][0-9a-fA-F]?)([0-9a-fA-F][0-9a-fA-F]?)\s*/;
 
     /**
