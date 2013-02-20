7f679673bd05e23bba9d7b5faa00318c037e1180
yiminghe <yiminghe@gmail.com>
2013-02-20 17:49:56 +0800

    support {{^

--- build/xtemplate/facade-min.js
+++ build/xtemplate/facade-min.js
@@ -1,7 +1,7 @@
 /*
 Copyright 2013, KISSY UI Library v1.30
 MIT Licensed
-build time: Jan 17 15:56
+build time: Feb 20 17:49
 */
 KISSY.add("xtemplate/facade",function(f,c,h){function a(b,d){d=f.merge(j,d);if("string"==typeof b){b=b.replace(/\{\{@/g,"{{#");var a=b,g=d,e;if(!g.cache||!(e=i[a]))e=h.compileToFn(a,g),g.cache&&(i[a]=e);b=e}this.option=d;this.tpl=b;this.runtime=new c(b,d)}var i=a.cache={};c.includeCommand.invokeEngine=function(b,d,c){return(new a(b,f.merge(c))).render(d,!0)};var j={cache:!0};f.augment(a,{removeSubTpl:function(b){this.runtime.removeSubTpl(b)},removeCommand:function(b){this.runtime.removeCommand(b)},
 addSubTpl:function(b,a){this.runtime.addSubTpl(b,a)},addCommand:function(b,a){this.runtime.addCommand(b,a)},render:function(a){return this.runtime.render.apply(this.runtime,arguments)}});a.compiler=h;a.RunTime=c;a.addCommand=c.addCommand;a.addSubTpl=c.addSubTpl;a.removeCommand=c.removeCommand;a.removeSubTpl=c.removeSubTpl;return a},{requires:["./runtime","./compiler"]});
