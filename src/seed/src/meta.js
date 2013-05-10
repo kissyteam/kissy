107aa31f37eb510c79acfa188a698f815157ac1a
yiminghe <yiminghe@gmail.com>
2013-05-10 16:42:48 +0800

    include cookie in core

--- src/seed/src/meta.js
+++ src/seed/src/meta.js
@@ -16,7 +16,7 @@
         },
         modules: {
             core: {
-                alias: ['dom', 'event', 'io', 'anim', 'base', 'node', 'json', 'ua']
+                alias: ['dom', 'event', 'io', 'anim', 'base', 'node', 'json', 'ua','cookie']
             }
         }
     });
