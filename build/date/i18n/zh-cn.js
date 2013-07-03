b25929f2ca073cdf1d64cf1d044481098b2ec23f
yiminghe <yiminghe@gmail.com>
2013-07-03 19:51:22 +0800

    add date build

---
+++ build/date/i18n/zh-cn.js
@@ -0,0 +1,24 @@
+﻿/*
+Copyright 2013, KISSY UI Library v1.40dev
+MIT Licensed
+build time: Jul 3 19:31
+*/
+/*
+ Combined processedModules by KISSY Module Compiler: 
+
+ date/i18n/zh-cn
+*/
+
+/**
+ * locale info for KISSY Date
+ * @author yiminghe@gmail.com
+ */
+KISSY.add('date/i18n/zh-cn', function () {
+    return {
+        // in minutes
+        timezoneOffset: 8 * 60,
+        firstDayOfWeek: 1,
+        minimalDaysInFirstWeek: 1
+    };
+});
+
