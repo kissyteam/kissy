85275301d25c45979f6e723f5baeee2efe3b8879
yiminghe <yiminghe@gmail.com>
2013-07-03 23:08:23 +0800

    add component demo

---
+++ src/component/demo/my-control/render.js
@@ -0,0 +1,29 @@
+KISSY.add(function (S, Control) {
+    return Control.ATTRS.xrender.value.extend({
+        beforeCreateDom: function (renderData, selectors) {
+            selectors.checkEl = '#ks-my-control-{id}';
+        },
+        _onSetChecked: function (v) {
+            var cls = this.getBaseCssClasses('checked');
+            this.control.get('checkEl')[v ? 'addClass' : 'removeClass'](cls);
+        }
+    }, {
+        HTML_PARSER: {
+            checkEl: function (el) {
+                return el.one('.' + this.getBaseCssClasses('check'));
+            }
+        },
+        ATTRS: {
+            contentTpl: {
+                value: '<div id="ks-my-control-{{id}}" class="{{getBaseCssClasses \'check\'}} ' +
+                    '{{#if checked}}' +
+                    '{{getBaseCssClass \'checked\'}}' +
+                    '{{/if}}' +
+                    '">' +
+                    '</div>'
+            }
+        }
+    });
+}, {
+    requires: ['component/control']
+});
