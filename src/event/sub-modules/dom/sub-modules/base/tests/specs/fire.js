5264e05637d42b8f8c7abc0e641810a0d829a55d
yiminghe <yiminghe@gmail.com>
2013-07-08 22:36:30 +0800

    add tc for #383

--- src/event/sub-modules/dom/sub-modules/base/tests/specs/fire.js
+++ src/event/sub-modules/dom/sub-modules/base/tests/specs/fire.js
@@ -81,6 +81,61 @@ KISSY.add(function (S, Dom, Event) {
             Dom.remove(n);
         });
 
+
+        it('fireHandler does not bubble', function () {
+
+            var n = Dom.create("<div>" +
+                "<div class='l1'>" +
+                "<div class='l2'></div>" +
+                "</div>" +
+                "</div>"),
+                ret = [],
+                dfn, winFn;
+
+            Dom.append(n, 'body');
+
+            var l1 = Dom.get('.l1', n);
+
+            var l2 = Dom.get('.l2', n);
+
+            Event.on(l1, 'click', function () {
+                ret.push(1);
+            });
+
+            Event.on(l2, 'click', function () {
+                ret.push(2);
+                Event.detach(l2);
+            });
+
+            Event.on(l2, 'click', function () {
+                Dom.append(l2, n);
+                ret.push(22);
+            });
+
+            Event.on(document, 'click', dfn = function () {
+                ret.push(3);
+            });
+
+            Event.on(window, 'click', winFn = function () {
+                ret.push(4);
+            });
+
+            Event.fireHandler(l2, 'click');
+
+            expect(ret).toEqual([2, 22]);
+
+            ret = [];
+
+            Event.fireHandler(l2, 'click');
+
+            expect(ret).toEqual([]);
+
+            Event.detach(document, 'click', dfn);
+
+            Event.detach(window, 'click', winFn);
+
+            Dom.remove(n);
+        });
     });
 
 },{
