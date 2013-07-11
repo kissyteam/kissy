5185025b33b561c8155b6935b738c31fa4b937fb
yiminghe <yiminghe@gmail.com>
2013-07-11 22:26:39 +0800

    add intl build

---
+++ build/intl/date/en-us.js
@@ -0,0 +1,31 @@
+﻿/*
+Copyright 2013, KISSY UI Library v1.40dev
+MIT Licensed
+build time: Jul 11 22:25
+*/
+/**
+ * locale info for KISSY Date
+ * @author yiminghe@gmail.com
+ */
+KISSY.add('intl/date/en-us', function () {
+    return {
+        // in minutes
+        timezoneOffset: -8 * 60,
+        firstDayOfWeek: 0,
+        minimalDaysInFirstWeek: 1,
+
+        // DateFormatSymbols
+        eras: ['BC', 'AD'],
+        months: ['January', 'February', 'March', 'April', 'May', 'June', 'July',
+            'August', 'September', 'October', 'November', 'December'],
+        shortMonths: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep',
+            'Oct', 'Nov', 'Dec'],
+        weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday',
+            'Saturday'],
+        shortWeekdays: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+        ampms: ['AM', 'PM'],
+        datePatterns:['EEEE, MMMM d, yyyy','MMMM d, yyyy','MMM d, yyyy','M/d/yy'],
+        timePatterns:['h:mm:ss \'GMT\'Z','h:mm:ss a \'GMT\'Z','h:mm:ss a','h:mm a'],
+        dateTimePattern:'{date} {time}'
+    };
+});
