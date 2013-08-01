878e41d20d9cc1a8551042a3c7d0a5924d1a536b
yiminghe <yiminghe@gmail.com>
2013-08-01 17:17:58 +0800

    update ua

--- build/kissy-nodejs.js
+++ build/kissy-nodejs.js
@@ -1,7 +1,7 @@
 ﻿/*
 Copyright 2013, KISSY UI Library v1.40dev
 MIT Licensed
-build time: Aug 1 12:44
+build time: Aug 1 17:17
 */
 /**
  * @ignore
@@ -39,11 +39,11 @@ var KISSY = (function (undefined) {
 
         /**
          * The build time of the library.
-         * NOTICE: '20130801124422' will replace with current timestamp when compressing.
+         * NOTICE: '20130801171725' will replace with current timestamp when compressing.
          * @private
          * @type {String}
          */
-        __BUILD_TIME: '20130801124422',
+        __BUILD_TIME: '20130801171725',
         /**
          * KISSY Environment.
          * @private
@@ -3110,7 +3110,7 @@ var KISSY = (function (undefined) {
 
     function getIEVersion(ua) {
         var m, v;
-        if ((m = ua.match(/MSIE ([^;]*)|Trident.*; rv ([0-9.]+)/)) &&
+        if ((m = ua.match(/MSIE ([^;]*)|Trident.*; rv(?:\d|:)?([0-9.]+)/)) &&
             (v = (m[1] || m[2]))) {
             return numberify(v);
         }
@@ -5656,7 +5656,7 @@ var KISSY = (function (undefined) {
             comboMaxFileNum: 40,
             charset: 'utf-8',
             lang: 'zh-cn',
-            tag: '20130801124422'
+            tag: '20130801171725'
         }, getBaseInfo()));
     }
 
