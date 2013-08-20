5a78d0e7747a124c93a5df64accf0ec10decf77d
yiminghe <yiminghe@gmail.com>
2013-08-20 22:08:10 +0800

    rebuild

---
+++ build/component/extension/shim-min.js
@@ -0,0 +1,6 @@
+/*
+Copyright 2013, KISSY UI Library v1.40dev
+MIT Licensed
+build time: Aug 20 17:44
+*/
+KISSY.add("component/extension/shim",function(a){function b(){}var a=6===a.UA.ie,c="<iframe style='position: absolute;border: none;width: "+(a?"expression(this.parentNode.clientWidth)":"100%")+";top: 0;opacity: 0;filter: alpha(opacity=0);left: 0;z-index: -1;height: "+(a?"expression(this.parentNode.clientHeight)":"100%")+";'/>";b.ATTRS={shim:{value:a}};b.prototype.__createDom=function(){this.get("shim")&&this.get("el").prepend(c)};return b});
---
+++ build/date/popup-picker.js
@@ -0,0 +1,43 @@
+/*
+Copyright 2013, KISSY UI Library v1.40dev
+MIT Licensed
+build time: Aug 20 17:37
+*/
+/*
+ Combined processedModules by KISSY Module Compiler: 
+
+ date/popup-picker
+*/
+
+/**
+ * popup date picker
+ * @author yiminghe@gmail.com
+ */
+KISSY.add('date/popup-picker', function (S, DatePicker, AlignExtension, Shim) {
+    var PopupDatePickerRender = DatePicker.getDefaultRender().extend({}, {
+        ATTRS: {
+            contentTpl: {
+                value: '<div ' +
+                    'class="{{getBaseCssClasses "content"}}">' +
+                    '{{{include "date/picker/picker-tpl"}}}' +
+                    '</div>'
+            }
+        }
+    });
+    return DatePicker.extend([Shim, AlignExtension], {
+    }, {
+        xclass: 'popup-date-picker',
+        ATTRS: {
+            xrender: {
+                value: PopupDatePickerRender
+            }
+        }
+    });
+}, {
+    requires: [
+        'date/picker',
+        'component/extension/align',
+        'component/extension/shim'
+    ]
+});
+
