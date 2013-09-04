16878eaf2cd6faa36991800ab558adc76bcaaf52
yiminghe <yiminghe@gmail.com>
2013-09-04 13:19:52 +0800

    update typo

--- build/seed.js
+++ build/seed.js
@@ -1,12 +1,12 @@
 /*
 Copyright 2013, KISSY UI Library v1.32
 MIT Licensed
-build time: Sep 4 13:13
+build time: Sep 4 13:19
 */
 /**
  * @ignore
  * A seed where KISSY grows up from, KISS Yeah !
- * @author https://github.com/kissyteam?tab=members
+ * @author https://github.com/kissyteam/kissy/contributors
  */
 
 /**
@@ -39,11 +39,11 @@ var KISSY = (function (undefined) {
 
         /**
          * The build time of the library.
-         * NOTICE: '20130904131340' will replace with current timestamp when compressing.
+         * NOTICE: '20130904131927' will replace with current timestamp when compressing.
          * @private
          * @type {String}
          */
-        __BUILD_TIME: '20130904131340',
+        __BUILD_TIME: '20130904131927',
         /**
          * KISSY Environment.
          * @private
@@ -5688,7 +5688,7 @@ var KISSY = (function (undefined) {
             // file limit number for a single combo url
             comboMaxFileNum: 40,
             charset: 'utf-8',
-            tag: '20130904131340'
+            tag: '20130904131927'
         }, getBaseInfo()));
     }
 
