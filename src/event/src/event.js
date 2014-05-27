4644b25aa99427f3a3f2226596acd85246884ee8
yiminghe <yiminghe@gmail.com>
2014-05-27 20:34:50 +0800

    deprecate event module

--- src/event/src/event.js
+++ src/event/src/event.js
@@ -6,5 +6,6 @@ KISSY.add(function (S, require) {
     var util = require('util');
     util.mix(Event, require('event/dom'));
     Event.Target = require('event/custom').Target;
+    S.log('event module is deprecated! please use \'event/dom\' or \'event/custom\' module instead.');
     return Event;
 });
