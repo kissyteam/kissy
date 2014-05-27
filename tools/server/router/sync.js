ed71dfb738ea5aa238716a019ae2485df9a742cf
yiminghe <yiminghe@gmail.com>
2014-05-27 17:07:42 +0800

    update sync

--- tools/server/router/sync.js
+++ tools/server/router/sync.js
@@ -1,5 +1,8 @@
 var serverConfig = require('../server.json');
 var exec = require('child_process').exec;
+var ncp = require('ncp').ncp;
+var path = require('path');
+var fs = require('fs');
 
 function sync(dir, callback, errorCallback) {
     exec('cd ' + dir + ' && git reset --hard && git pull origin && git merge origin/master',
@@ -13,13 +16,52 @@ function sync(dir, callback, errorCallback) {
         });
 }
 
+function cloneBranch(dir, branch, callback, errorCallback) {
+    exec('git clone https://github.com/kissyteam/docs.kissyui.com -b ' + branch + ' ' + path.join(dir, branch),
+        { maxBuffer: 1024 * 1024 },
+        function (error, stdout, stderr) {
+            if (error) {
+                errorCallback(error);
+            } else {
+                callback(stdout || stderr);
+            }
+        });
+}
+
+function updateBranch(dir, branch, callback, errorCallback) {
+    exec('cd ' + path.join(dir, branch) + ' && git pull origin ' + branch,
+        { maxBuffer: 1024 * 1024 },
+        function (error, stdout, stderr) {
+            if (error) {
+                errorCallback(error);
+            } else {
+                callback(stdout || stderr);
+            }
+        });
+}
+
+function syncBranch(dir, branch, callback, errorCallback) {
+    if (!fs.existsSync(path.join(dir, branch))) {
+        cloneBranch(dir, branch, function () {
+            updateBranch(dir, branch, callback, errorCallback);
+        }, errorCallback);
+    } else {
+        updateBranch(dir, branch, callback, errorCallback);
+    }
+}
+
 function syncCode(callback, errorCallback) {
     sync(serverConfig.codeDir, callback, errorCallback);
 }
 
 function syncDocs(callback, errorCallback) {
-    sync(serverConfig.docsDir, function () {
-        sync(serverConfig.docsDir + '/5.0', callback, errorCallback);
+    sync(serverConfig.docsDir, callback, errorCallback);
+}
+
+function syncDocsVersion(version, callback, errorCallback) {
+    syncBranch(serverConfig.newDocsDir, version, function () {
+        ncp(path.join(serverConfig.newDocsDir, version + '/build'),
+            path.join(serverConfig.docsDir, version), callback);
     }, errorCallback);
 }
 
@@ -34,10 +76,36 @@ function shouldSyncFn(req) {
     return shouldSync;
 }
 
+function getSyncBranch(req) {
+    var branch = '';
+    if (req.body && req.body.payload) {
+        var payload = JSON.parse(req.body.payload);
+        branch = payload.ref.match(/^refs\/heads\/(\d+\.\d+)$/);
+        if (branch) {
+            branch = branch[1];
+        }
+    }
+    return branch;
+}
+
 module.exports = function (app) {
     app.all('/sync', function (req, res) {
         if (shouldSyncFn(req)) {
             syncCode(function (str) {
+                res.send(str);
+            }, function (str) {
+                res.status(500);
+                res.send(str);
+            });
+        } else {
+            res.end();
+        }
+    });
+
+    app.all('/sync-new-docs', function (req, res) {
+        var branch = getSyncBranch(req);
+        if (branch) {
+            syncDocsVersion(branch, function (str) {
                 res.send(str);
             }, function (str) {
                 res.status(500);
