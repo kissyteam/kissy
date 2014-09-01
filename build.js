f883defdda196eddb67c7b55925568b6974ccb01
yiminghe <yiminghe@gmail.com>
2014-09-01 17:01:49 +0800

    remove util

--- build.js
+++ build.js
@@ -1,10 +1,20 @@
 var fs = require('fs-extra');
 var path = require('path');
-var bowerDir = path.resolve(process.cwd(),'bower_components');
-var xtemplateDir =  path.resolve(bowerDir,'xtemplate/build/');
-var buildDir = path.resolve(process.cwd(),'build');
-fs.copySync(path.resolve(xtemplateDir,'xtemplate-debug.js'),path.resolve(buildDir,'xtemplate-debug.js'));
-fs.copySync(path.resolve(xtemplateDir,'xtemplate.js'),path.resolve(buildDir,'xtemplate.js'));
-fs.copySync(path.resolve(xtemplateDir,'xtemplate/runtime-debug.js'),path.resolve(buildDir,'xtemplate/runtime-debug.js'));
-fs.copySync(path.resolve(xtemplateDir,'xtemplate/runtime.js'),path.resolve(buildDir,'xtemplate/runtime.js'));
+var bowerDir = path.resolve(process.cwd(), 'bower_components');
+var buildDir = path.resolve(process.cwd(), 'build');
+
+function build(component, components) {
+    var cDir = path.resolve(bowerDir, component + '/build/');
+    if (!components) {
+        components = [component];
+    }
+    components.forEach(function (c) {
+        fs.copySync(path.resolve(cDir, c + '-debug.js'), path.resolve(buildDir, c + '-debug.js'));
+        fs.copySync(path.resolve(cDir, c + '.js'), path.resolve(buildDir, c + '.js'));
+    });
+}
+
+build('xtemplate', ['xtemplate', 'xtemplate/runtime']);
+build('util');
+
 console.log('done');
--- src/util/tests/runner/test.jss
+++
@@ -1,7 +0,0 @@
-module.exports = function (req, res) {
-    res.render('runner', {
-        loader:true,
-        component: 'util',
-        query: req.query
-    });
-};
