7b80310b99bf095ec6b846fe356f41ce9e0f4f83
yiminghe <yiminghe@gmail.com>
2014-10-09 19:19:56 +0800

    remove build

--- build/separator.js
+++
@@ -1,6 +0,0 @@
-/*
-Copyright 2014, KISSY v5.0.0
-MIT Licensed
-build time: Aug 26 16:10
-*/
-KISSY.add("separator",["component/control"],function(a,b,d,c){a=b("component/control");c.exports=a.extend({beforeCreateDom:function(a){a.elAttrs.role="separator"}},{ATTRS:{handleGestureEvents:{value:!1},focusable:{value:!1},allowTextSelection:{value:!1},disabled:{value:!0}},xclass:"separator"})});
