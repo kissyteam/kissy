49677701f3fb30ab295856500fe98cd290fce4ce
yiminghe <yiminghe@gmail.com>
2014-10-14 10:53:18 +0800

    add infer base from seed

--- build.js
+++ build.js
@@ -3,23 +3,23 @@ var path = require('path');
 var bowerDir = path.resolve(process.cwd(), 'bower_components');
 var buildDir = path.resolve(process.cwd(), 'build');
 var aggregateBower = require('aggregate-bower');
+var inferBaseCode = '\nmodulex.init({name:"seed"});';
 
-function generateSeedJs(){
-	var seedDebugJsContent = '',
-		seedJsContent = '',
-		filesList = ['modulex', 'feature', 'ua', 'meta'];
-
-	for(var i = 0; i < filesList.length; i++){
-		var debugFilePath = path.join(buildDir, filesList[i] + '-debug.js'),
-			miniFilePath = path.join(buildDir, filesList[i] + '.js');
-		seedDebugJsContent += fs.readFileSync(debugFilePath).toString();
-		seedJsContent += fs.readFileSync(miniFilePath).toString();
-	}
-	fs.writeFileSync(path.join(buildDir, 'seed-debug.js'), seedDebugJsContent);
-	fs.writeFileSync(path.join(buildDir, 'seed.js'), seedJsContent);
+function generateSeedJs() {
+    var seedDebugJsContent = '',
+        seedJsContent = '',
+        filesList = ['modulex', 'feature', 'ua', 'meta'];
+    for (var i = 0; i < filesList.length; i++) {
+        var debugFilePath = path.join(buildDir, filesList[i] + '-debug.js'),
+            miniFilePath = path.join(buildDir, filesList[i] + '.js');
+        seedDebugJsContent += fs.readFileSync(debugFilePath).toString();
+        seedJsContent += fs.readFileSync(miniFilePath).toString();
+    }
+    fs.writeFileSync(path.join(buildDir, 'seed-debug.js'), seedDebugJsContent + inferBaseCode);
+    fs.writeFileSync(path.join(buildDir, 'seed.js'), seedJsContent + inferBaseCode);
 }
 
-aggregateBower(bowerDir,buildDir);
+aggregateBower(bowerDir, buildDir);
 generateSeedJs();
 
 console.log('done...');
